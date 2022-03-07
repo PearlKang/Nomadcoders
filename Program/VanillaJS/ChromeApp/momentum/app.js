5bc81f3b54d7e50ba30eba356b6a1cea035662a8
PearlKang <rksk1588@gmail.com>
2022-03-08 00:14:34 +0900

    2022.03.08

---
+++ Program/VanillaJS/ChromeApp/momentum/app.js
@@ -0,0 +1,16 @@
+console.log(123123123);
+console.log("testestest");
+console.log(5+2);
+console.log(5*2);
+console.log(5/2);
+
+const a = 5;
+const b = 2;
+const myName = "nico";
+const veryLongVariableName = 0;
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+
+console.log("hello " + myName);
