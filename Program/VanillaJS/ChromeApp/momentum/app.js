673eedcccddb9a4e60e81597d9611c70e44c0c15
PearlKang <rksk1588@gmail.com>
2022-03-09 21:48:21 +0900

    2022.03.09

--- Program/VanillaJS/ChromeApp/momentum/app.js
+++ Program/VanillaJS/ChromeApp/momentum/app.js
@@ -54,3 +54,31 @@ console.log(daysOfWeek3);
 const toBuy = ["potato", "tomato", "pizza"];
 toBuy.push("kimbab");
 console.log(toBuy[2]);
+
+const playerName = "nico";
+const playerPoints = 121212;
+const playerHandsome = false;
+const playerFat = "little bit";
+
+// player[0] == name
+// player[1] == points
+const player1 = ["nico", 1212, false, "little bit"];
+
+const player = {
+    name: "nico", 
+    points: 10,
+    fat: true,
+}
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+
+player.fat = false;
+console.log(player);
+
+player.lastName = "potato";
+player.points = 16;
+console.log(player);
+
+player.points = player.points + 16;
+console.log(player);
