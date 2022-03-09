00c5bdb6286894f59bdd1a16c6a2ed1efc7ba60e
PearlKang <rksk1588@gmail.com>
2022-03-10 00:49:47 +0900

    2022.03.10

--- Program/VanillaJS/ChromeApp/momentum/app.js
+++ Program/VanillaJS/ChromeApp/momentum/app.js
@@ -1,8 +1,8 @@
 console.log(123123123);
 console.log("testestest");
-console.log(5+2);
-console.log(5*2);
-console.log(5/2);
+console.log(5 + 2);
+console.log(5 * 2);
+console.log(5 / 2);
 
 const a = 5;
 const b = 2;
@@ -13,12 +13,10 @@ console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 
-
 let myNames = "nico";
 console.log("hello " + myNames);
 myNames = "nicolas";
 console.log("your new name is " + myNames);
-
 
 const amIFat = false;
 const amIFat2 = null;
@@ -26,7 +24,7 @@ const amIFat3 = undefined;
 console.log(amIFat);
 
 let something;
-console.log(something, amIFat); 
+console.log(something, amIFat);
 
 const mon = "mon";
 const tue = "tue";
@@ -57,7 +55,6 @@ const toBuy = ["potato", "tomato", "pizza"];
 toBuy.push("kimbab");
 console.log(toBuy[2]);
 
-
 const playerName = "nico";
 const playerPoints = 121212;
 const playerHandsome = false;
@@ -68,10 +65,10 @@ const playerFat = "little bit";
 const player1 = ["nico", 1212, false, "little bit"];
 
 const player = {
-    name: "nico", 
-    points: 10,
-    fat: true,
-}
+  name: "nico",
+  points: 10,
+  fat: true,
+};
 console.log(player);
 console.log(player.name);
 console.log(player["name"]);
@@ -86,15 +83,14 @@ console.log(player);
 player.points = player.points + 16;
 console.log(player);
 
-
 function sayHello() {
-    console.log("Hello!");
+  console.log("Hello!");
 }
 
 sayHello();
 
 function sayHello2(nameOfPerson) {
-    console.log("Hello my name is " + nameOfPerson);
+  console.log("Hello my name is " + nameOfPerson);
 }
 
 sayHello2("nico");
@@ -102,65 +98,61 @@ sayHello2("dal");
 sayHello2("lynn");
 
 function sayHello3(nameOfPerson, age) {
-    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+  console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
 }
 
 sayHello3("nico", 10);
 sayHello3("dal", 23);
 sayHello3("lynn", 21);
 
-
 function plus1(a, b) {
-    console.log(a, b);
+  console.log(a, b);
 }
 
 plus1();
 
 function plus2(a, b) {
-    console.log(a + b);
+  console.log(a + b);
 }
 
 plus2();
 plus2(8, 60);
 
 function plus3(firstNumber, secondNumber) {
-    console.log(firstNumber + secondNumber);
+  console.log(firstNumber + secondNumber);
 }
 
 plus3();
 plus3(8, 60);
 
-
 function plus(firstNumber, secondNumber) {
-    console.log(firstNumber + secondNumber);
+  console.log(firstNumber + secondNumber);
 }
 
 // console.log(firstNumber);
 
 function divide(a, b) {
-    console.log(a / b);
+  console.log(a / b);
 }
 
 plus(60, 8);
-divide(98,20);
-
+divide(98, 20);
 
 const players = {
-    name: "nico",
-    sayHello: function() {
-        console.log("hello!");
-    },
-    sayHello2: function(otherPersonsName) {
-        console.log("hello " + otherPersonsName + " nice to meet you!");
-    },
-}
+  name: "nico",
+  sayHello: function () {
+    console.log("hello!");
+  },
+  sayHello2: function (otherPersonsName) {
+    console.log("hello " + otherPersonsName + " nice to meet you!");
+  },
+};
 
 console.log(players.name);
 //console.log(players.sayHello);
 //console.log(players.sayHello());
 players.sayHello();
 players.sayHello2("lynn");
-
 
 const aa = 5;
 console.log(aa);
@@ -186,3 +178,44 @@ console.log(days[2]);
 
 days.push("ones");
 console.log(days);
+
+const user = {
+  name: "Nico",
+  age: 98,
+};
+
+console.log(user, console);
+console.log((user.name = "nicolas"));
+console.log(user);
+
+user.name = "ben";
+console.log(user);
+
+user.sexy = "soon";
+console.log(user);
+
+function plus4() {
+  console.log(2 + 2);
+}
+
+plus4();
+
+function plus5(a, b) {
+  console.log(a + b);
+}
+
+plus5(2, 2);
+
+function minusFive(potato) {
+  console.log(potato - 5);
+}
+
+minusFive(1, 2, 3, 3, 45235, 4);
+
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+};
+
+calculator.add(1, 2);
