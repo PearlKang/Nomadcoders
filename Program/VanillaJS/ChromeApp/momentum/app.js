3bcdcfb95ec5292f2d2ac2422ff7d3e63dfa177f
PearlKang <rksk1588@gmail.com>
2022-03-11 16:50:35 +0900

    2022.03.11

--- Program/VanillaJS/ChromeApp/momentum/app.js
+++ Program/VanillaJS/ChromeApp/momentum/app.js
@@ -283,8 +283,16 @@ if (condition) {
 }
 */
 
-if (isNaN(ages)) {
-  console.log("Please write a number.");
+if (isNaN(ages) || ages < 0) {
+  console.log("Please write a real positive number.");
+} else if (ages < 18) {
+  console.log("You are too young.");
+} else if (ages >= 18 && ages <= 50) {
+  console.log("You can drink.");
+} else if (ages > 50 && ages <= 80) {
+  console.log("You should exercise.");
+} else if (ages > 80) {
+  console.log("You can do whatever you want.");
 } else {
-  console.log("Thank you for writing your age.");
+  console.log("You can't drink.");
 }
