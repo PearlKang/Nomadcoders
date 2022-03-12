12bc8c1fda7a6533d2fa9ae38e373ce8b6074019
PearlKang <rksk1588@gmail.com>
2022-03-12 19:27:56 +0900

    2022.03.12

--- Program/VanillaJS/ChromeApp/momentum/app.js
+++ Program/VanillaJS/ChromeApp/momentum/app.js
@@ -335,45 +335,14 @@ title.innerText = "Hello";
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handelTitleClick() {
-  if (h1.style.color == "blue") {
-    h1.style.color = "red";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
   } else {
-    h1.style.color = "blue";
+    newColor = "blue";
   }
-}
-
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("ALL GOOOD");
+  h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handelTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-//title.onclick = handelTitleClick;
-//title.onmouseenter = handleMouseEnter;
-//title.onmouseleave = handleMouseLeave;
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
