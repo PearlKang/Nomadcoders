346cc12a74e550730d3d2c44d3f6940c0f065fa2
PearlKang <rksk1588@gmail.com>
2022-03-12 19:45:08 +0900

    2022.03.12

--- Program/VanillaJS/ChromeApp/momentum/app.js
+++ Program/VanillaJS/ChromeApp/momentum/app.js
@@ -2,10 +2,10 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 function handelTitleClick() {
   const clickedClass = "clicked";
-  if (h1.className === clickedClass) {
-    h1.className = "";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
   } else {
-    h1.className = clickedClass;
+    h1.classList.add(clickedClass);
   }
 }
 
