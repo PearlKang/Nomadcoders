33774f3aa24ba3b3e00b820ec27e9384af74823d
PearlKang <rksk1588@gmail.com>
2022-03-12 19:19:10 +0900

    2022.03.12

--- Program/VanillaJS/ChromeApp/momentum/app.js
+++ Program/VanillaJS/ChromeApp/momentum/app.js
@@ -332,24 +332,48 @@ const title = document.querySelector(".hello h1");
 
 title.innerText = "Hello";
 */
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handelTitleClick() {
-  if (title.style.color == "blue") {
-    title.style.color = "red";
+  if (h1.style.color == "blue") {
+    h1.style.color = "red";
   } else {
-    title.style.color = "blue";
+    h1.style.color = "blue";
   }
 }
 
 function handleMouseEnter() {
-  title.innerText = "Mouse is here!";
+  h1.innerText = "Mouse is here!";
 }
 
 function handleMouseLeave() {
-  title.innerText = "Mouse is gone!";
+  h1.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handelTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("ALL GOOOD");
+}
+
+h1.addEventListener("click", handelTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+//title.onclick = handelTitleClick;
+//title.onmouseenter = handleMouseEnter;
+//title.onmouseleave = handleMouseLeave;
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
