f180d51eaa86dc02ca39fd685a0b1ffafd03ca36
PearlKang <rksk1588@gmail.com>
2022-03-14 15:46:56 +0900

    2022.03.14

--- Program/VanillaJS/ChromeApp/momentum/app.js
+++ Program/VanillaJS/ChromeApp/momentum/app.js
@@ -13,13 +13,7 @@ const loginButton = document.querySelector("#login-form button");
 //handleLoginBtnClic
 function onLoginBtnClick() {
   const username = loginInput.value;
-
-  if (username === "") {
-    //console.log("Please write your name");
-    alert("Please write your name");
-  } else if (username.length > 15) {
-    alert("Your name is too long.");
-  }
+  console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
