0288864ee239389a9400ae1df359c4824a8d8ec2
PearlKang <rksk1588@gmail.com>
2022-03-14 20:40:05 +0900

    2022.03.14

--- Program/VanillaJS/ChromeApp/momentum/app.js
+++ Program/VanillaJS/ChromeApp/momentum/app.js
@@ -10,10 +10,18 @@ const loginButton = loginForm.querySelector("button");
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 //handleLoginBtnClick
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
