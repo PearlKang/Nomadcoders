948f71272ca67e836243f057ed8acabacc314c81
PearlKang <rksk1588@gmail.com>
2022-03-15 21:23:43 +0900

    2022.03.15

--- Program/VanillaJS/ChromeApp/momentum/js/clock.js
+++ Program/VanillaJS/ChromeApp/momentum/js/clock.js
@@ -1,8 +1,9 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-//setInterval(sayHello, 5000);
-setTimeout(sayHello, 5000);
+getClock();
+setInterval(getClock, 1000);
