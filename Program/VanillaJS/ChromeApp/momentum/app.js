a93c38fa77ed90ab79d3e5667afe9122fe3edf01
PearlKang <rksk1588@gmail.com>
2022-03-15 20:42:07 +0900

    2022.03.15

--- Program/VanillaJS/ChromeApp/momentum/app.js
+++ Program/VanillaJS/ChromeApp/momentum/app.js
@@ -18,9 +18,9 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username);
+  const usernameThatTheUserWrote = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, usernameThatTheUserWrote);
+  paintGreetings(usernameThatTheUserWrote);
 }
 
 function paintGreetings(username) {
