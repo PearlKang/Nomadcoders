3b9cfb922c07ac162c6a62c64a0ec8996a5bd6ad
PearlKang <rksk1588@gmail.com>
2022-03-15 20:09:03 +0900

    2022.03.15

--- Program/VanillaJS/ChromeApp/momentum/app.js
+++ Program/VanillaJS/ChromeApp/momentum/app.js
@@ -9,13 +9,17 @@ const loginButton = loginForm.querySelector("button");
 */
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 //handleLoginBtnClick
 function onLoginSubmit(event) {
   event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  loginForm.classList.add("hidden");
-  console.log(username);
+  greeting.innerText = "Hello " + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
