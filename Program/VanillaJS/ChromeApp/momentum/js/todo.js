e5cec077c4febb51b7d37ad5d2c0762aacb6285d
PearlKang <rksk1588@gmail.com>
2022-03-16 18:28:33 +0900

    2022.03.16

--- Program/VanillaJS/ChromeApp/momentum/js/todo.js
+++ Program/VanillaJS/ChromeApp/momentum/js/todo.js
@@ -18,8 +18,9 @@ function deleteToDo(event) {
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
@@ -32,8 +33,12 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
   saveToDos();
 }
 
