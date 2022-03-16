155bdce647c7152cae8e8e5371b67c87bc051475
PearlKang <rksk1588@gmail.com>
2022-03-16 20:05:44 +0900

    2022.03.16

--- Program/VanillaJS/ChromeApp/momentum/js/weather.js
+++ Program/VanillaJS/ChromeApp/momentum/js/weather.js
@@ -1,6 +1,10 @@
+const API_KEY = "79db836a232fee8e104a536695be4ea5";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  fetch(url);
 }
 
 function onGeoError() {
