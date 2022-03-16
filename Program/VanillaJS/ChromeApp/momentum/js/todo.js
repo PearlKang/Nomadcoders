5d4987ee6e375371c8d299d47bfcc38f80aa5198
PearlKang <rksk1588@gmail.com>
2022-03-16 19:42:30 +0900

    2022.03.16

--- Program/VanillaJS/ChromeApp/momentum/js/todo.js
+++ Program/VanillaJS/ChromeApp/momentum/js/todo.js
@@ -14,6 +14,8 @@ function saveToDos() {
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintToDo(newTodo) {
