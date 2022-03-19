1861a577c1d57ccfdfc94bdbd4ab0945145fb81d
PearlKang <rksk1588@gmail.com>
2022-03-19 16:28:39 +0900

    2022.03.19

--- Clones/Wetube/wetube/src/server.js
+++ Clones/Wetube/wetube/src/server.js
@@ -4,9 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log("I'm in the middle!");
-  console.log(`Someone is going to: ${req.url}`);
+const loggerMiddleware = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
   next();
 };
 
@@ -18,9 +26,15 @@ const handleLogin = (req, res) => {
   return res.send({ message: "Login here." });
 };
 
-app.use(gossipMiddleware);
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+app.use(loggerMiddleware);
+app.use(privateMiddleware);
 app.get("/", handleHome);
 app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`✔ Server listening on port http://localhost:${PORT} 🚀`);
