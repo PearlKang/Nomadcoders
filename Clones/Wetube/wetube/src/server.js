8c49fb9c6be8678d46f4e721de5c008003d27b92
PearlKang <rksk1588@gmail.com>
2022-03-19 15:27:53 +0900

    2022.03.19

--- Clones/Wetube/wetube/src/server.js
+++ Clones/Wetube/wetube/src/server.js
@@ -3,9 +3,11 @@ import express from "express";
 const PORT = 4000;
 
 const app = express();
-/*
-app.listen(4000, () => console.log("Server listening on port 4000 🚀"));
-*/
+
+const handleHome = () => console.log("Somebody is trying to go home.");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`✔ Server listening on port http://localhost:${PORT} 🚀`);
 
