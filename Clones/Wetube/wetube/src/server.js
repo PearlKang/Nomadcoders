c8f13788dcbfc2f24933f77907797b1a59066cd1
PearlKang <rksk1588@gmail.com>
2022-03-19 15:45:46 +0900

    2022.03.19

--- Clones/Wetube/wetube/src/server.js
+++ Clones/Wetube/wetube/src/server.js
@@ -4,9 +4,18 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("Somebody is trying to go home.");
+const handleHome = (req, res) => {
+  return res.send("I still love you.");
+  //return res.end();
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
+  //return res.end();
+};
 
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`✔ Server listening on port http://localhost:${PORT} 🚀`);
