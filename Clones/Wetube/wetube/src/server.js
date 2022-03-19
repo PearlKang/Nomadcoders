097bf1ac022e2b4117a2a8314d3457616e311243
PearlKang <rksk1588@gmail.com>
2022-03-19 15:54:12 +0900

    2022.03.19

--- Clones/Wetube/wetube/src/server.js
+++ Clones/Wetube/wetube/src/server.js
@@ -5,13 +5,11 @@ const PORT = 4000;
 const app = express();
 
 const handleHome = (req, res) => {
-  return res.send("I still love you.");
-  //return res.end();
+  return res.send("<h1>I still love you.</h1>");
 };
 
 const handleLogin = (req, res) => {
-  return res.send("Login here.");
-  //return res.end();
+  return res.send({ message: "Login here." });
 };
 
 app.get("/", handleHome);
