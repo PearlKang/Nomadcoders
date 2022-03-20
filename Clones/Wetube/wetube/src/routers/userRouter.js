180fec7b766e8486bc629a40b6665c0dfbca1c85
PearlKang <rksk1588@gmail.com>
2022-03-21 00:25:37 +0900

    2022.03.21

--- Clones/Wetube/wetube/src/routers/userRouter.js
+++ Clones/Wetube/wetube/src/routers/userRouter.js
@@ -6,6 +6,6 @@ const userRouter = express.Router();
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
-userRouter.get("/:id", see);
+userRouter.get("/:id(\\d+)", see);
 
 export default userRouter;
