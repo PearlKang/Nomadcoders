6a8f49456f05603383c56643e1a55bcaa967144c
PearlKang <rksk1588@gmail.com>
2022-03-22 22:47:02 +0900

    2022.03.22

--- Clones/Wetube/wetube/src/controllers/videoController.js
+++ Clones/Wetube/wetube/src/controllers/videoController.js
@@ -1,8 +1,6 @@
 export const trending = (req, res) => res.render("home");
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => {
-  return res.send("Edit");
-};
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
