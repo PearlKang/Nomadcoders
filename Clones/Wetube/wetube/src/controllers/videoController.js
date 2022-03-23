6b73398ecb476c35753acaecd424b59beb70d68b
PearlKang <rksk1588@gmail.com>
2022-03-23 23:33:29 +0900

    2022.03.23

--- Clones/Wetube/wetube/src/controllers/videoController.js
+++ Clones/Wetube/wetube/src/controllers/videoController.js
@@ -3,8 +3,10 @@ const fakeUser = {
   loggedIn: false,
 };
 
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return res.render("home", { pageTitle: "Home", fakeUser, videos });
+};
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
