7567182c06a7af290bc652bdc18e232d31d8198f
PearlKang <rksk1588@gmail.com>
2022-03-26 17:38:59 +0900

    2022.03.26

--- Clones/Wetube/wetube/src/controllers/videoController.js
+++ Clones/Wetube/wetube/src/controllers/videoController.js
@@ -61,12 +61,13 @@ export const getUpload = (req, res) => {
 export const postUpload = (req, res) => {
   const { title } = req.body;
   const newVideo = {
-    title: title,
+    title,
     rating: 0,
     comments: 0,
     createdAt: "just now",
     views: 0,
     id: videos.length + 1,
   };
+  videos.push(newVideo);
   return res.redirect("/");
 };
