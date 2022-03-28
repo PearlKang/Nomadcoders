0e4a480fc1a5f2907bb1a543d9f630dfea4383da
PearlKang <rksk1588@gmail.com>
2022-03-28 23:35:39 +0900

    2022.03.28

--- Clones/Wetube/wetube/src/controllers/videoController.js
+++ Clones/Wetube/wetube/src/controllers/videoController.js
@@ -1,8 +1,11 @@
 import Video from "../models/Video";
 
 export const home = (req, res) => {
-  Video.find({}, (error, videos) => {});
-  return res.render("home", { pageTitle: "Home" });
+  Video.find({}, (error, videos) => {
+    console.log("errors", error);
+    console.log("videos", videos);
+  });
+  return res.render("home", { pageTitle: "Home", videos: [] });
 };
 
 export const watch = (req, res) => {
