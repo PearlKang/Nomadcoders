1f44f085fdf8122834a84620b3d664675d568b42
PearlKang <rksk1588@gmail.com>
2022-03-28 22:30:38 +0900

    2022.03.28

--- Clones/Wetube/wetube/src/routers/globalRouter.js
+++ Clones/Wetube/wetube/src/routers/globalRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { join, login } from "../controllers/userController";
-import { trending, search } from "../controllers/videoController";
+import { trending } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
