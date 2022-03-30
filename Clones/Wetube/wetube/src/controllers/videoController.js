4c5165688f955841cd6d21bf9587cf5c1dfcb229
PearlKang <rksk1588@gmail.com>
2022-03-31 01:15:34 +0900

    2022.03.31

--- Clones/Wetube/wetube/src/controllers/videoController.js
+++ Clones/Wetube/wetube/src/controllers/videoController.js
@@ -28,7 +28,7 @@ export const getUpload = (req, res) => {
 
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
+  await Video.create({
     title,
     description,
     createdAt: Date.now(),
@@ -38,6 +38,5 @@ export const postUpload = async (req, res) => {
       rating: 0,
     },
   });
-  await video.save();
   return res.redirect("/");
 };
