bb692826dc8ddcea58f9255d5e16490d9cbadbe9
PearlKang <rksk1588@gmail.com>
2022-04-02 15:43:35 +0900

    2022.04.02

--- Clones/Wetube/wetube/src/controllers/videoController.js
+++ Clones/Wetube/wetube/src/controllers/videoController.js
@@ -3,22 +3,26 @@ import Video from "../models/Video";
 export const home = async (req, res) => {
   const videos = await Video.find({});
   console.log(videos);
+
   return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const watch = (req, res) => {
   const { id } = req.params;
+
   return res.render("watch", { pageTitle: `Watching:` });
 };
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
+
   return res.render("edit", { pageTitle: `Editing:` });
 };
 
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
+
   return res.redirect(`/videos/${id}`);
 };
 
@@ -28,12 +32,14 @@ export const getUpload = (req, res) => {
 
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
+
   try {
     await Video.create({
       title,
       description,
       hashtags: hashtags.split(",").map((word) => `#${word}`),
     });
+
     return res.redirect("/");
   } catch (error) {
     return res.render("upload", {
