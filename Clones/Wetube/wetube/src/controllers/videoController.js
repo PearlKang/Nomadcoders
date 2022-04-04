072e0a8e6e2081c8990bf8492072f10515d5d557
PearlKang <rksk1588@gmail.com>
2022-04-05 00:08:06 +0900

    2022.04.05

--- Clones/Wetube/wetube/src/controllers/videoController.js
+++ Clones/Wetube/wetube/src/controllers/videoController.js
@@ -33,7 +33,7 @@ export const getEdit = async (req, res) => {
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  const video = await Video.findById(id);
+  const video = await Video.exists({ _id: id });
 
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
