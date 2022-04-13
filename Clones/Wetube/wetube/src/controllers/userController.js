661798efa2b5ebaa9f15b3e0c12f815983a0cdf1
PearlKang <rksk1588@gmail.com>
2022-04-14 00:29:34 +0900

    2022.04.14

--- Clones/Wetube/wetube/src/controllers/userController.js
+++ Clones/Wetube/wetube/src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 
@@ -45,19 +46,28 @@ export const getLogin = (req, res) =>
 
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
+  const pageTitle = "Login";
+  const user = await User.findOne({ username });
 
-  // check if account exists
-  const exists = await User.exists({ username });
-
-  if (!exists) {
+  if (!user) {
     return res.status(400).render("login", {
-      pageTitle: "Login",
+      pageTitle,
       errorMessage: "An account with this username does not exists.",
     });
   }
 
-  // check if password correct
-  res.end();
+  const ok = await bcrypt.compare(password, user.password);
+
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password",
+    });
+  }
+
+  console.log("LOG USER IN! COMING SOON!");
+
+  return res.redirect("/");
 };
 
 export const edit = (req, res) => res.send("Edit User");
