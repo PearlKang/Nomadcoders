891bfc85a781e08fa09fe576226154549543c7b8
PearlKang <rksk1588@gmail.com>
2022-04-16 15:49:21 +0900

    2022.04.16

--- Clones/Wetube/wetube/src/controllers/userController.js
+++ Clones/Wetube/wetube/src/controllers/userController.js
@@ -65,7 +65,8 @@ export const postLogin = async (req, res) => {
     });
   }
 
-  console.log("LOG USER IN! COMING SOON!");
+  req.session.loggedIn = true;
+  req.session.user = user;
 
   return res.redirect("/");
 };
