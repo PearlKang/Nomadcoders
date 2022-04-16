7db5765f11830e3ccae568c3b9f715410f7afa48
PearlKang <rksk1588@gmail.com>
2022-04-16 16:26:32 +0900

    2022.04.16

--- Clones/Wetube/wetube/src/middlewares.js
+++ Clones/Wetube/wetube/src/middlewares.js
@@ -1,6 +1,7 @@
 export const localsMiddleware = (req, res, next) => {
-  console.log(req.session);
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
+  res.locals.loggedInUser = req.session.user;
+
   next();
 };
