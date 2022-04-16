1cb5c3ae117275f4b8cb1f3d316dbb5871aa3469
PearlKang <rksk1588@gmail.com>
2022-04-16 15:51:52 +0900

    2022.04.16

--- Clones/Wetube/wetube/src/server.js
+++ Clones/Wetube/wetube/src/server.js
@@ -31,11 +31,6 @@ app.use((req, res, next) => {
   });
 });
 
-app.get("/add-one", (req, res, next) => {
-  req.session.potato += 1;
-  return res.send(`${req.session.id}\n${req.session.potato}`);
-});
-
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
