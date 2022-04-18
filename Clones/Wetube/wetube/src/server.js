2d4fe176fe37ead426e6df02b6bd25b8d5b02855
PearlKang <rksk1588@gmail.com>
2022-04-19 00:22:28 +0900

    2022.04.19

--- Clones/Wetube/wetube/src/server.js
+++ Clones/Wetube/wetube/src/server.js
@@ -24,6 +24,9 @@ app.use(
     secret: "Hello!",
     resave: false,
     saveUninitialized: false,
+    cookie: {
+      maxAge: 20000,
+    },
     store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
   })
 );
