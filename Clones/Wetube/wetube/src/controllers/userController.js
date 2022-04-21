e2a23d9f6ebf1146bba7091d539a0b0a79a0d801
PearlKang <rksk1588@gmail.com>
2022-04-22 01:09:06 +0900

    2022.04.22

--- Clones/Wetube/wetube/src/controllers/userController.js
+++ Clones/Wetube/wetube/src/controllers/userController.js
@@ -105,14 +105,23 @@ export const finishGithubLogin = async (req, res) => {
 
   if ("access_token" in tokenRequest) {
     const { access_token } = tokenRequest;
-    const userRequest = await (
-      await fetch("https://api.github.com/user", {
+    const apiUrl = "https://api.github.com";
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
         headers: {
           Authorization: `token ${access_token}`,
         },
       })
     ).json();
-    console.log(userRequest);
+    console.log(userData);
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(emailData);
   } else {
     return res.redirect("/login");
   }
