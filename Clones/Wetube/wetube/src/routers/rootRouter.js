445400eed1227bea4e51532142bd3fd4628003d4
PearlKang <rksk1588@gmail.com>
2022-04-26 21:49:01 +0900

    2022.04.26

--- Clones/Wetube/wetube/src/routers/rootRouter.js
+++ Clones/Wetube/wetube/src/routers/rootRouter.js
@@ -8,11 +8,17 @@ import {
 } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 
+import { publicOnlyMiddleware } from "../middlewares";
+
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+rootRouter
+  .route("/login")
+  .all(publicOnlyMiddleware)
+  .get(getLogin)
+  .post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
--- Clones/Wetube/wetube/src/routers/userRouter.js
+++ Clones/Wetube/wetube/src/routers/userRouter.js
@@ -9,17 +9,14 @@ import {
   finishGithubLogin,
 } from "../controllers/userController";
 
-import { protectorMiddleware } from "../middlewares";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
-userRouter
-  .route("/edit")
-  .get(protectorMiddleware, getEdit)
-  .post(protectorMiddleware, postEdit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/:id(\\d+)", watch);
 
 export default userRouter;
