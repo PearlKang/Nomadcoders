661acb43cba16956c015f4a324a95f41ff4b53e7
PearlKang <rksk1588@gmail.com>
2022-04-26 21:40:23 +0900

    2022.04.26

--- Clones/Wetube/wetube/src/middlewares.js
+++ Clones/Wetube/wetube/src/middlewares.js
@@ -5,3 +5,19 @@ export const localsMiddleware = (req, res, next) => {
 
   next();
 };
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};
--- Clones/Wetube/wetube/src/routers/userRouter.js
+++ Clones/Wetube/wetube/src/routers/userRouter.js
@@ -9,10 +9,15 @@ import {
   finishGithubLogin,
 } from "../controllers/userController";
 
+import { protectorMiddleware } from "../middlewares";
+
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.route("/edit").get(getEdit).post(postEdit);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter
+  .route("/edit")
+  .get(protectorMiddleware, getEdit)
+  .post(protectorMiddleware, postEdit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id(\\d+)", watch);
