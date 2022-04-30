87647662cb86cace3a6d95d3d2d48737f214aec6
PearlKang <rksk1588@gmail.com>
2022-04-30 15:47:11 +0900

    2022.04.30

--- Clones/Wetube/wetube/src/controllers/userController.js
+++ Clones/Wetube/wetube/src/controllers/userController.js
@@ -194,9 +194,36 @@ export const getChangePassword = (req, res) => {
   return res.render("users/change-password", { pageTitle: "Change Password" });
 };
 
-export const postChangePassword = (req, res) => {
+export const postChangePassword = async (req, res) => {
+  const {
+    session: {
+      user: { _id, password },
+    },
+    body: { oldPassword, newPassword, newPasswordConfirmation },
+  } = req;
+
+  const ok = await bcrypt.compare(oldPassword, password);
+
+  if (!ok) {
+    return res.status(400).render("users/change-password", {
+      pageTitle: "Change Password",
+      errorMessage: "The current password is incorrect",
+    });
+  }
+
+  if (newPassword !== newPasswordConfirmation) {
+    return res.status(400).render("users/change-password", {
+      pageTitle: "Change Password",
+      errorMessage: "The password does not match the confirmation",
+    });
+  }
+
+  const user = await User.findById(_id);
+  user.password = newPassword;
+  await user.save();
+  req.session.user.password = user.password;
   // send notification
-  return res.redirect("/");
+  return res.redirect("/users/logout");
 };
 
 export const watch = (req, res) => res.send("Watch User");
