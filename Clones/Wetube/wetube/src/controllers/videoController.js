c4e9d5c6fc1e17cec9c2057447bf8457ef75861b
PearlKang <rksk1588@gmail.com>
2022-05-02 21:31:55 +0900

    2022.05.02

--- Clones/Wetube/wetube/src/controllers/videoController.js
+++ Clones/Wetube/wetube/src/controllers/videoController.js
@@ -51,14 +51,14 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req, res) => {
-  const { path } = req.file;
+  const { path: fileUrl } = req.file;
   const { title, description, hashtags } = req.body;
 
   try {
     await Video.create({
       title,
       description,
-      fileUrl: path,
+      fileUrl,
       hashtags: Video.formatHashtags(hashtags),
     });
 
