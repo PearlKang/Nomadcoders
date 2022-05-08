0787039529dbef13ede96a1eef539fbe387d1c66
PearlKang <rksk1588@gmail.com>
2022-05-08 23:00:24 +0900

    2022.05.08
    webpack debug

--- Clones/Wetube/wetube/src/server.js
+++ Clones/Wetube/wetube/src/server.js
@@ -31,7 +31,7 @@ app.use(
 app.use(localsMiddleware);
 
 app.use("/uploads", express.static("uploads"));
-app.use("/assets", express.static("assets"));
+app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
--- Clones/Wetube/wetube/webpack.config.js
+++ Clones/Wetube/wetube/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader "],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
