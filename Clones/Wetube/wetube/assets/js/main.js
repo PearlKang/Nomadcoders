6f21f7cdcd451c0c7f72009274a0f630ad6af217
PearlKang <rksk1588@gmail.com>
2022-05-11 00:29:34 +0900

    2022.05.11

--- Clones/Wetube/wetube/assets/js/main.js
+++ Clones/Wetube/wetube/assets/js/main.js
@@ -16,7 +16,7 @@
   \*******************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./src/client/scss/styles.scss\");\n\nconsole.log(\"hi\");\n\n//# sourceURL=webpack://wetube/./src/client/js/main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./src/client/scss/styles.scss\");\n\nconsole.log(\"main\");\n\n//# sourceURL=webpack://wetube/./src/client/js/main.js?");
 
 /***/ }),
 
