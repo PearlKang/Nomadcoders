a270313fbed7d0ba7c983feda3ed97f1ee9e9100
PearlKang <rksk1588@gmail.com>
2022-05-13 00:44:33 +0900

    2022.05.13

--- Clones/Wetube/wetube/src/client/js/videoPlayer.js
+++ Clones/Wetube/wetube/src/client/js/videoPlayer.js
@@ -1,1 +1,22 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handelPlay = (e) => {
+  // if the video is playing, pause it
+  if (video.paused) {
+    playBtn.innerText = "Pause";
+    video.play();
+  } else {
+    playBtn.innerText = "Play";
+    video.pause();
+  }
+  // else play the video.
+};
+
+const handelMute = (e) => {};
+
+playBtn.addEventListener("click", handelPlay);
+muteBtn.addEventListener("click", handelMute);
