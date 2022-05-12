33602687057a2afdcc00cd24439304f2d525cffa
PearlKang <rksk1588@gmail.com>
2022-05-13 00:48:46 +0900

    2022.05.13

--- Clones/Wetube/wetube/src/client/js/videoPlayer.js
+++ Clones/Wetube/wetube/src/client/js/videoPlayer.js
@@ -4,19 +4,22 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volume = document.getElementById("volume");
 
-const handelPlay = (e) => {
+const handelPlayClick = (e) => {
   // if the video is playing, pause it
   if (video.paused) {
-    playBtn.innerText = "Pause";
     video.play();
   } else {
-    playBtn.innerText = "Play";
     video.pause();
   }
   // else play the video.
 };
 
+const handelPause = () => (playBtn.innerText = "Play");
+const handelPlay = () => (playBtn.innerText = "Pause");
+
 const handelMute = (e) => {};
 
-playBtn.addEventListener("click", handelPlay);
+playBtn.addEventListener("click", handelPlayClick);
 muteBtn.addEventListener("click", handelMute);
+video.addEventListener("pause", handelPause);
+video.addEventListener("play", handelPlay);
