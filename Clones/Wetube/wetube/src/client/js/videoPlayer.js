a73a453beb87f0e1b0b946125ebdb7607efe3cac
PearlKang <rksk1588@gmail.com>
2022-05-15 16:16:18 +0900

    2022.05.15

--- Clones/Wetube/wetube/src/client/js/videoPlayer.js
+++ Clones/Wetube/wetube/src/client/js/videoPlayer.js
@@ -2,24 +2,26 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 const handelPlayClick = (e) => {
-  // if the video is playing, pause it
   if (video.paused) {
     video.play();
   } else {
     video.pause();
   }
-  // else play the video.
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handelPause = () => (playBtn.innerText = "Play");
-const handelPlay = () => (playBtn.innerText = "Pause");
-
-const handelMute = (e) => {};
+const handelMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : 0.5;
+};
 
 playBtn.addEventListener("click", handelPlayClick);
 muteBtn.addEventListener("click", handelMute);
-video.addEventListener("pause", handelPause);
-video.addEventListener("play", handelPlay);
