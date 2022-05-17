ce4ca598e57850dd44fa6248afaa113f87572122
PearlKang <rksk1588@gmail.com>
2022-05-18 01:58:27 +0900

    2022.05.18

--- Clones/Wetube/wetube/src/routers/apiRouter.js
+++ Clones/Wetube/wetube/src/routers/apiRouter.js
@@ -4,6 +4,6 @@ import { registerView } from "../controllers/videoController";
 
 const apiRouter = express.Router();
 
-apiRouter.post("videos/:id([0-9a-f]{24})/view", registerView);
+apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
 
 export default apiRouter;
