c0001e28e85db06942083c1588fc0745ab482d42
PearlKang <rksk1588@gmail.com>
2022-05-18 00:48:57 +0900

    2022.05.18

--- Clones/Wetube/wetube/src/controllers/videoController.js
+++ Clones/Wetube/wetube/src/controllers/videoController.js
@@ -142,3 +142,17 @@ export const search = async (req, res) => {
 
   return res.render("search", { pageTitle: "Search", videos });
 };
+
+export const registerView = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+
+  if (!video) {
+    return res.status(404);
+  }
+
+  video.meta.views += 1;
+  await video.save();
+
+  return res.status(200);
+};
--- Clones/Wetube/wetube/src/server.js
+++ Clones/Wetube/wetube/src/server.js
@@ -6,6 +6,7 @@ import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import apiRouter from "./routers/apiRouter";
 
 import { localsMiddleware } from "./middlewares";
 
@@ -35,5 +36,6 @@ app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
+app.use("/api", apiRouter);
 
 export default app;
