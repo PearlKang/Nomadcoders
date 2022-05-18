7d641aef1dcfb7ba779ca34ac4b6a2dec4973d2f
PearlKang <rksk1588@gmail.com>
2022-05-18 22:43:53 +0900

    2022.05.18

--- Clones/Wetube/wetube/src/client/js/recorder.js
+++ Clones/Wetube/wetube/src/client/js/recorder.js
@@ -1,10 +1,14 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const handleStart = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
+
+  video.srcObject = stream;
+  video.play();
 };
 
 startBtn.addEventListener("click", handleStart);
