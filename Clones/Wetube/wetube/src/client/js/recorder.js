b590ec0df87e7014b301e65824c286ebb739894c
PearlKang <rksk1588@gmail.com>
2022-05-19 00:03:19 +0900

    2022.05.19

--- Clones/Wetube/wetube/src/client/js/recorder.js
+++ Clones/Wetube/wetube/src/client/js/recorder.js
@@ -3,8 +3,17 @@ const video = document.getElementById("preview");
 
 let stream;
 let recorder;
+let videoFile;
 
-const handleDownload = () => {};
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm";
+
+  document.body.appendChild(a);
+
+  a.click();
+};
 
 const handleStop = () => {
   startBtn.innerText = "Download Recording";
@@ -19,10 +28,9 @@ const handleStart = () => {
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
 
-  recorder = new MediaRecorder(stream);
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
   recorder.ondataavailable = (event) => {
-    const videoFile = URL.createObjectURL(event.data);
-
+    videoFile = URL.createObjectURL(event.data);
     video.srcObject = null;
     video.src = videoFile;
     video.loop = true;
