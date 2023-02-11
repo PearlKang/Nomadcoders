4a2d9e278ea1a85036d46f0b4dde4d7c79bf6eca
BenKang <rksk1588@gmail.com>
2023-02-11 22:26:12 +0900

    2023.02.11

--- Program/Flutter/toonflix/lib/main.dart
+++ Program/Flutter/toonflix/lib/main.dart
@@ -14,20 +14,28 @@ class App extends StatelessWidget {
           backgroundColor: Colors.black,
           body: Column(
             children: [
+              SizedBox(
+                height: 80,
+              ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
                       Text(
                         "Hey, Selena",
                         style: TextStyle(
                           color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
                         ),
                       ),
                       Text(
                         "Welcome back",
                         style: TextStyle(
-                          color: Colors.white,
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
                         ),
                       ),
                     ],
