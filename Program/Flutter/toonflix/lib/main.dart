89a8f525f2904ebd2ef67af1c4d123e9020705a8
BenKang <rksk1588@gmail.com>
2023-02-17 01:08:33 +0900

    2023.02.17

--- Program/Flutter/toonflix/lib/main.dart
+++ Program/Flutter/toonflix/lib/main.dart
@@ -119,6 +119,7 @@ class App extends StatelessWidget {
                   height: 20,
                 ),
                 Container(
+                  clipBehavior: Clip.hardEdge,
                   decoration: BoxDecoration(
                     color: const Color(0xFF1F2123),
                     borderRadius: BorderRadius.circular(25),
@@ -165,10 +166,16 @@ class App extends StatelessWidget {
                             ),
                           ],
                         ),
-                        const Icon(
-                          Icons.euro_rounded,
-                          color: Colors.white,
-                          size: 98,
+                        Transform.scale(
+                          scale: 2.2,
+                          child: Transform.translate(
+                            offset: const Offset(-5, 12),
+                            child: const Icon(
+                              Icons.euro_rounded,
+                              color: Colors.white,
+                              size: 88,
+                            ),
+                          ),
                         ),
                       ],
                     ),
