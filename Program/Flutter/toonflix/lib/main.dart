c0855f93b3c0df5d0a016bcb8bb48c2578b40318
BenKang <rksk1588@gmail.com>
2023-02-23 00:24:11 +0900

    2023.02.23

--- Program/Flutter/toonflix/lib/main.dart
+++ Program/Flutter/toonflix/lib/main.dart
@@ -12,6 +12,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,8 +35,12 @@ class _AppState extends State<App> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              MyLargeTitle(),
+            children: [
+              showTitle ? const MyLargeTitle() : const Text("nothing"),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: const Icon(Icons.remove_red_eye),
+              ),
             ],
           ),
         ),
@@ -37,18 +49,36 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    super.initState();
+    print("initState!");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose!");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print("build!");
     return Text(
       "My Large Title",
       style: TextStyle(
         fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge!.color,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
