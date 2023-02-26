02c4b71e957c3e5dc16d4164f83ab7f2ada1b1ad
BenKang <rksk1588@gmail.com>
2023-02-27 01:11:48 +0900

    2023.02.27

--- Program/Flutter/toonflix/lib/models/webtoon_model.dart
+++ Program/Flutter/toonflix/lib/models/webtoon_model.dart
@@ -1,9 +1,8 @@
 class WebtoonModel {
   final String title, thumb, id;
 
-  WebtoonModel({
-    required this.title,
-    required this.thumb,
-    required this.id,
-  });
+  WebtoonModel.fromJson(Map<String, dynamic> json)
+      : title = json["title"],
+        thumb = json["thumb"],
+        id = json["id"];
 }
--- Program/Flutter/toonflix/lib/services/api_service.dart
+++ Program/Flutter/toonflix/lib/services/api_service.dart
@@ -1,19 +1,22 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
   void getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse("$baseUrl/$today");
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
-        print(webtoon);
+        final toon = WebtoonModel.fromJson(webtoon);
+        print(toon.title);
       }
       return;
     }
