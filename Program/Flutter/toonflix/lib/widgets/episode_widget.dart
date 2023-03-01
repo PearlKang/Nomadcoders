6ededf0dc742a6430cea7cc5e989789bb8a7de40
BenKang <rksk1588@gmail.com>
2023-03-02 01:04:11 +0900

    2023.03.02

--- Program/Flutter/toonflix/lib/widgets/episode_widget.dart
+++ Program/Flutter/toonflix/lib/widgets/episode_widget.dart
@@ -6,12 +6,15 @@ class Episode extends StatelessWidget {
   const Episode({
     super.key,
     required this.episode,
+    required this.webtoonId,
   });
 
+  final String webtoonId;
   final WebtoonEpisodeModel episode;
 
   onButtonTap() async {
-    await launchUrlString("https://google.com");
+    await launchUrlString(
+        "https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${episode.id}");
   }
 
   @override
