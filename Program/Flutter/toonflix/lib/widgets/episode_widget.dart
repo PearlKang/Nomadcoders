9f6cd7133881a773dacc23b04896b131dd76f10d
BenKang <rksk1588@gmail.com>
2023-03-02 00:57:18 +0900

    2023.03.02

--- Program/Flutter/toonflix/lib/widgets/episode_widget.dart
+++ Program/Flutter/toonflix/lib/widgets/episode_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_episode_model.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class Episode extends StatelessWidget {
   const Episode({
@@ -11,52 +11,53 @@ class Episode extends StatelessWidget {
   final WebtoonEpisodeModel episode;
 
   onButtonTap() async {
-    // await launchUrlString("https://google.com");
-    final url = Uri.parse("https://google.com");
-    await launchUrl(url);
+    await launchUrlString("https://google.com");
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(
-        bottom: 10,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.white,
-        border: Border.all(
-          color: Colors.green.shade400,
-          width: 1.5,
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        margin: const EdgeInsets.only(
+          bottom: 10,
         ),
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 3,
-            offset: const Offset(5, 5),
-            color: Colors.black.withOpacity(0.5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+          border: Border.all(
+            color: Colors.green.shade400,
+            width: 1.5,
           ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 10,
-          horizontal: 20,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              episode.title,
-              style: TextStyle(
-                color: Colors.green.shade400,
-                fontSize: 16,
-              ),
-            ),
-            Icon(
-              Icons.chevron_right_rounded,
-              color: Colors.green.shade400,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 3,
+              offset: const Offset(5, 5),
+              color: Colors.black.withOpacity(0.5),
             ),
           ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 20,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                episode.title,
+                style: TextStyle(
+                  color: Colors.green.shade400,
+                  fontSize: 16,
+                ),
+              ),
+              Icon(
+                Icons.chevron_right_rounded,
+                color: Colors.green.shade400,
+              ),
+            ],
+          ),
         ),
       ),
     );
