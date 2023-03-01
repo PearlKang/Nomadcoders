bafd1d31846f10863ef2642f097f53a87b3039f1
BenKang <rksk1588@gmail.com>
2023-03-01 17:18:35 +0900

    2023.03.01

---
+++ Program/Flutter/toonflix/lib/widgets/webtoon_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class Webtoon extends StatelessWidget {
+  final String title, thumb, id;
+
+  const Webtoon({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print("take me homeeeeeeee");
+      },
+      child: Column(
+        children: [
+          Container(
+            width: 250,
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 15,
+                  offset: const Offset(10, 10),
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ],
+            ),
+            child: Image.network(
+              thumb,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 22,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
