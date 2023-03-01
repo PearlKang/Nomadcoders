127313fab6d3aad990104f89392b76359b7f214e
BenKang <rksk1588@gmail.com>
2023-03-01 17:44:54 +0900

    2023.03.01

--- Program/Flutter/toonflix/lib/widgets/webtoon_widget.dart
+++ Program/Flutter/toonflix/lib/widgets/webtoon_widget.dart
@@ -29,21 +29,24 @@ class Webtoon extends StatelessWidget {
       },
       child: Column(
         children: [
-          Container(
-            width: 250,
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 15,
-                  offset: const Offset(10, 10),
-                  color: Colors.black.withOpacity(0.5),
-                ),
-              ],
-            ),
-            child: Image.network(
-              thumb,
+          Hero(
+            tag: id,
+            child: Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ],
+              ),
+              child: Image.network(
+                thumb,
+              ),
             ),
           ),
           const SizedBox(
