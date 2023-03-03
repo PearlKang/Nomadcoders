4c0e6009bf613cce7677ae432ae9e7748fc04b5d
BenKang <rksk1588@gmail.com>
2023-03-03 22:53:44 +0900

    2023.03.03

--- Clones/Tiktok/tiktok_clone/lib/main.dart
+++ Clones/Tiktok/tiktok_clone/lib/main.dart
@@ -12,7 +12,7 @@ class TikTokApp extends StatelessWidget {
     return MaterialApp(
       title: 'TikTok Clone',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFFE9435A),
       ),
       home: Container(),
     );
