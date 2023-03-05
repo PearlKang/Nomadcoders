f9b9eb676d7dfd5d0648d5beed2fa6c1bce3e8ff
BenKang <rksk1588@gmail.com>
2023-03-05 16:44:24 +0900

    2023.03.05
    Sign up screen
    bottom nav bar

--- Clones/Tiktok/tiktok_clone/lib/main.dart
+++ Clones/Tiktok/tiktok_clone/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tiktok_clone/constants/gaps.dart';
-import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/screens/sign_up/sign_up_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -16,18 +15,7 @@ class TikTokApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xFFE9435A),
       ),
-      home: Padding(
-        padding: const EdgeInsets.all(Sizes.size14),
-        child: Container(
-          child: Row(
-            children: const [
-              Text("hello"),
-              Gaps.h20,
-              Text("hello"),
-            ],
-          ),
-        ),
-      ),
+      home: const SignUpScreen(),
     );
   }
 }
