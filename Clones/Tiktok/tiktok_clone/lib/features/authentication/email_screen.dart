919e9a02f3929c27825bb9de15489d76f2f66f9d
BenKang <rksk1588@gmail.com>
2023-03-08 02:11:28 +0900

    2023.03.08

--- Clones/Tiktok/tiktok_clone/lib/features/authentication/email_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/authentication/email_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class EmailScreen extends StatelessWidget {
   const EmailScreen({super.key});
@@ -9,6 +11,32 @@ class EmailScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text(
           "Sign up",
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Gaps.v40,
+            Text(
+              "Create username",
+              style: TextStyle(
+                fontSize: Sizes.size24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v8,
+            Text(
+              "You can always change this later.",
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black54,
+              ),
+            ),
+          ],
         ),
       ),
     );
