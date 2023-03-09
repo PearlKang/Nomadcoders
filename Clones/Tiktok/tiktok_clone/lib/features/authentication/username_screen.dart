da660c59a8c485a1643be07a18d9a345c178afc4
BenKang <rksk1588@gmail.com>
2023-03-10 00:47:52 +0900

    2023.03.10

--- Clones/Tiktok/tiktok_clone/lib/features/authentication/username_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/authentication/username_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
 
 class UsernameScreen extends StatefulWidget {
   const UsernameScreen({super.key});
@@ -77,50 +78,6 @@ class _UsernameScreenState extends State<UsernameScreen> {
             Gaps.v16,
             FormButton(disabled: _username.isEmpty),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class FormButton extends StatelessWidget {
-  const FormButton({
-    super.key,
-    required this.disabled,
-  });
-
-  final bool disabled;
-
-  @override
-  Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 1,
-      child: AnimatedContainer(
-        padding: const EdgeInsets.symmetric(
-          vertical: Sizes.size16,
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(
-            Sizes.size5,
-          ),
-          color:
-              disabled ? Colors.grey.shade300 : Theme.of(context).primaryColor,
-        ),
-        duration: const Duration(
-          milliseconds: 300,
-        ),
-        child: AnimatedDefaultTextStyle(
-          duration: const Duration(
-            milliseconds: 300,
-          ),
-          style: TextStyle(
-            color: disabled ? Colors.grey.shade400 : Colors.white,
-            fontWeight: FontWeight.w600,
-          ),
-          child: const Text(
-            "Next",
-            textAlign: TextAlign.center,
-          ),
         ),
       ),
     );
---
+++ Clones/Tiktok/tiktok_clone/lib/features/authentication/widgets/form_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class FormButton extends StatelessWidget {
+  const FormButton({
+    super.key,
+    required this.disabled,
+  });
+
+  final bool disabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: AnimatedContainer(
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size16,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(
+            Sizes.size5,
+          ),
+          color:
+              disabled ? Colors.grey.shade300 : Theme.of(context).primaryColor,
+        ),
+        duration: const Duration(
+          milliseconds: 300,
+        ),
+        child: AnimatedDefaultTextStyle(
+          duration: const Duration(
+            milliseconds: 300,
+          ),
+          style: TextStyle(
+            color: disabled ? Colors.grey.shade400 : Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+          child: const Text(
+            "Next",
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
