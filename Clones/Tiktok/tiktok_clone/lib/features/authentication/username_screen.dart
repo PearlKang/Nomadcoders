54609e3d4f0e65deea88cba24fa7ab12748b3d8e
BenKang <rksk1588@gmail.com>
2023-03-10 00:45:25 +0900

    2023.03.10

--- Clones/Tiktok/tiktok_clone/lib/features/authentication/username_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/authentication/username_screen.dart
@@ -75,40 +75,52 @@ class _UsernameScreenState extends State<UsernameScreen> {
               cursorColor: Theme.of(context).primaryColor,
             ),
             Gaps.v16,
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: AnimatedContainer(
-                padding: const EdgeInsets.symmetric(
-                  vertical: Sizes.size16,
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(
-                    Sizes.size5,
-                  ),
-                  color: _username.isEmpty
-                      ? Colors.grey.shade300
-                      : Theme.of(context).primaryColor,
-                ),
-                duration: const Duration(
-                  milliseconds: 300,
-                ),
-                child: AnimatedDefaultTextStyle(
-                  duration: const Duration(
-                    milliseconds: 300,
-                  ),
-                  style: TextStyle(
-                    color:
-                        _username.isEmpty ? Colors.grey.shade400 : Colors.white,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  child: const Text(
-                    "Next",
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
+            FormButton(disabled: _username.isEmpty),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class FormButton extends StatelessWidget {
+  const FormButton({
+    super.key,
+    required this.disabled,
+  });
+
+  final bool disabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: AnimatedContainer(
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size16,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(
+            Sizes.size5,
+          ),
+          color:
+              disabled ? Colors.grey.shade300 : Theme.of(context).primaryColor,
+        ),
+        duration: const Duration(
+          milliseconds: 300,
+        ),
+        child: AnimatedDefaultTextStyle(
+          duration: const Duration(
+            milliseconds: 300,
+          ),
+          style: TextStyle(
+            color: disabled ? Colors.grey.shade400 : Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+          child: const Text(
+            "Next",
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
