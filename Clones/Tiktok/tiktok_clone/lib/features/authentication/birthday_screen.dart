291c37f9cd0d8f04d0b2613f77f6e9f57cfe783d
BenKang <rksk1588@gmail.com>
2023-03-14 01:04:04 +0900

    2023.03.14

--- Clones/Tiktok/tiktok_clone/lib/features/authentication/birthday_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/authentication/birthday_screen.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/authentication/email_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 class BirthdayScreen extends StatefulWidget {
   const BirthdayScreen({super.key});
@@ -14,15 +15,12 @@ class BirthdayScreen extends StatefulWidget {
 class _BirthdayScreenState extends State<BirthdayScreen> {
   final TextEditingController _birthdayController = TextEditingController();
 
-  DateTime date = DateTime.now();
+  DateTime initialDate = DateTime.now();
 
   @override
   void initState() {
     super.initState();
-    final textDate = date.toString().split(" ").first;
-    _birthdayController.value = TextEditingValue(
-      text: textDate,
-    );
+    _setTextFieldDate(initialDate);
   }
 
   @override
@@ -34,8 +32,15 @@ class _BirthdayScreenState extends State<BirthdayScreen> {
   void _onNextTap() {
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => const EmailScreen(),
+        builder: (context) => const InterestsScreen(),
       ),
+    );
+  }
+
+  void _setTextFieldDate(DateTime date) {
+    final textDate = date.toString().split(" ").first;
+    _birthdayController.value = TextEditingValue(
+      text: textDate,
     );
   }
 
@@ -96,6 +101,17 @@ class _BirthdayScreenState extends State<BirthdayScreen> {
               ),
             ),
           ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: SizedBox(
+          height: 300,
+          child: CupertinoDatePicker(
+            maximumDate: initialDate,
+            initialDateTime: initialDate,
+            mode: CupertinoDatePickerMode.date,
+            onDateTimeChanged: _setTextFieldDate,
+          ),
         ),
       ),
     );
