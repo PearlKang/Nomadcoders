9eba96d7ec9da359753d33395f9ff7a75c634938
BenKang <rksk1588@gmail.com>
2023-03-15 00:39:07 +0900

    2023.03.15

--- Clones/Tiktok/tiktok_clone/lib/features/authentication/login_form_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/authentication/login_form_screen.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
 
-class LoginFormScreen extends StatelessWidget {
+class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
 
   @override
+  State<LoginFormScreen> createState() => _LoginFormScreenState();
+}
+
+class _LoginFormScreenState extends State<LoginFormScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Log in"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Gaps.v28,
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Email",
+                ),
+                validator: (value) {
+                  return "i dont like your email";
+                },
+              ),
+              Gaps.v16,
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Password",
+                ),
+                validator: (value) {
+                  return "wrong password";
+                },
+              ),
+              Gaps.v28,
+              const FormButton(disabled: false),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
