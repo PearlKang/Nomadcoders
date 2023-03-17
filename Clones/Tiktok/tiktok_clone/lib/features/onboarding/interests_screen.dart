58c1fc09a02753ebc6cb1059fcfd84e982453a77
BenKang <rksk1588@gmail.com>
2023-03-17 22:33:08 +0900

    2023.03.17

--- Clones/Tiktok/tiktok_clone/lib/features/onboarding/interests_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/onboarding/interests_screen.dart
@@ -5,6 +5,10 @@ class InterestsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Choose your interests"),
+      ),
+    );
   }
 }
