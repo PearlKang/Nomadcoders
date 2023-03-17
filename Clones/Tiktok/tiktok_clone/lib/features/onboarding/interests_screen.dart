605e3e95711affc9b6908af140549c18ffa671f1
BenKang <rksk1588@gmail.com>
2023-03-17 22:57:08 +0900

    2023.03.17

--- Clones/Tiktok/tiktok_clone/lib/features/onboarding/interests_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/onboarding/interests_screen.dart
@@ -52,56 +52,67 @@ class InterestsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Choose your interests"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: Sizes.size24,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Gaps.v32,
-            const Text(
-              "Choose your interests",
-              style: TextStyle(
-                fontSize: Sizes.size40,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Gaps.v20,
-            const Text(
-              "Get better video recommendations",
-              style: TextStyle(
-                fontSize: Sizes.size20,
-              ),
-            ),
-            Gaps.v64,
-            for (var interest in interests)
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  vertical: Sizes.size12,
-                  horizontal: Sizes.size24,
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(
-                    Sizes.size32,
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.05),
-                      blurRadius: 5,
-                      spreadRadius: 5,
-                    )
-                  ],
-                ),
-                child: Text(
-                  interest,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size24,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v32,
+              const Text(
+                "Choose your interests",
+                style: TextStyle(
+                  fontSize: Sizes.size40,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-          ],
+              Gaps.v20,
+              const Text(
+                "Get better video recommendations",
+                style: TextStyle(
+                  fontSize: Sizes.size20,
+                ),
+              ),
+              Gaps.v64,
+              Wrap(
+                runSpacing: 15,
+                spacing: 15,
+                children: [
+                  for (var interest in interests)
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: Sizes.size16,
+                        horizontal: Sizes.size24,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(
+                          Sizes.size32,
+                        ),
+                        border: Border.all(
+                          color: Colors.black.withOpacity(0.1),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.05),
+                            blurRadius: 5,
+                            spreadRadius: 5,
+                          )
+                        ],
+                      ),
+                      child: Text(
+                        interest,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
