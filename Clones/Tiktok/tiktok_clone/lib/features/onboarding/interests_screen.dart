6a8b6ef8a5efb7068bec1e768a9ae9160adfc00c
BenKang <rksk1588@gmail.com>
2023-03-17 22:48:20 +0900

    2023.03.17

--- Clones/Tiktok/tiktok_clone/lib/features/onboarding/interests_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/onboarding/interests_screen.dart
@@ -1,4 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+const interests = [
+  "Daily Life",
+  "Comedy",
+  "Entertainment",
+  "Animals",
+  "Food",
+  "Beauty & Style",
+  "Drama",
+  "Learning",
+  "Talent",
+  "Sports",
+  "Auto",
+  "Family",
+  "Fitness & Health",
+  "DIY & Life Hacks",
+  "Arts & Crafts",
+  "Dance",
+  "Outdoors",
+  "Oddly Satisfying",
+  "Home & Garden",
+  "Daily Life",
+  "Comedy",
+  "Entertainment",
+  "Animals",
+  "Food",
+  "Beauty & Style",
+  "Drama",
+  "Learning",
+  "Talent",
+  "Sports",
+  "Auto",
+  "Family",
+  "Fitness & Health",
+  "DIY & Life Hacks",
+  "Arts & Crafts",
+  "Dance",
+  "Outdoors",
+  "Oddly Satisfying",
+  "Home & Garden",
+];
 
 class InterestsScreen extends StatelessWidget {
   const InterestsScreen({super.key});
@@ -8,6 +51,58 @@ class InterestsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Choose your interests"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size24,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v32,
+            const Text(
+              "Choose your interests",
+              style: TextStyle(
+                fontSize: Sizes.size40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Gaps.v20,
+            const Text(
+              "Get better video recommendations",
+              style: TextStyle(
+                fontSize: Sizes.size20,
+              ),
+            ),
+            Gaps.v64,
+            for (var interest in interests)
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size12,
+                  horizontal: Sizes.size24,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(
+                    Sizes.size32,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.05),
+                      blurRadius: 5,
+                      spreadRadius: 5,
+                    )
+                  ],
+                ),
+                child: Text(
+                  interest,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
