b19229109755df9c8c41ee8588d6a3a58f44f827
BenKang <rksk1588@gmail.com>
2023-03-20 20:42:48 +0900

    2023.03.20

---
+++ Clones/Tiktok/tiktok_clone/lib/features/onboarding/tutorial_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TutorialScreen extends StatefulWidget {
+  const TutorialScreen({super.key});
+
+  @override
+  State<TutorialScreen> createState() => _TutorialScreenState();
+}
+
+class _TutorialScreenState extends State<TutorialScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
