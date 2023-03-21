19bbdce0a23fc7776d6a8c9ad36b274ce09deeaf
BenKang <rksk1588@gmail.com>
2023-03-21 18:52:56 +0900

    2023.03.21

--- Clones/Tiktok/tiktok_clone/lib/features/onboarding/tutorial_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/onboarding/tutorial_screen.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
-
-// Text(
-//   "Watch cool videos!",
-//   style: TextStyle(
-//     fontSize: Sizes.size40,
-//     fontWeight: FontWeight.bold,
-//   ),
-// ),
-// Gaps.v16,
-// Text(
-//   "Videos are personalized for you based on what you watch, like, and share.",
-//   style: TextStyle(
-//     fontSize: Sizes.size20,
-//   ),
-// ),
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class TutorialScreen extends StatefulWidget {
   const TutorialScreen({super.key});
@@ -23,8 +10,69 @@ class TutorialScreen extends StatefulWidget {
 }
 
 class _TutorialScreenState extends State<TutorialScreen> {
+  void _onPanUpdate(DragUpdateDetails details) {
+    print(details);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector();
+    return GestureDetector(
+      onPanUpdate: _onPanUpdate,
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size24,
+          ),
+          child: SafeArea(
+            child: AnimatedCrossFade(
+              firstChild: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Gaps.v80,
+                  Text(
+                    "Watch cool videos!",
+                    style: TextStyle(
+                      fontSize: Sizes.size40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gaps.v16,
+                  Text(
+                    "Videos are personalized for you based on what you watch, like, and share.",
+                    style: TextStyle(
+                      fontSize: Sizes.size20,
+                    ),
+                  ),
+                ],
+              ),
+              secondChild: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Gaps.v80,
+                  Text(
+                    "Follow the rules",
+                    style: TextStyle(
+                      fontSize: Sizes.size40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gaps.v16,
+                  Text(
+                    "Videos are personalized for you based on what you watch, like, and share.",
+                    style: TextStyle(
+                      fontSize: Sizes.size20,
+                    ),
+                  ),
+                ],
+              ),
+              crossFadeState: CrossFadeState.showSecond,
+              duration: const Duration(
+                milliseconds: 300,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
