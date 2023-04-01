1ad0869e8b9943b6b0a2637770fdd8a5af3c638c
BenKang <rksk1588@gmail.com>
2023-04-01 21:43:32 +0900

    2023.04.01
    PageController

---
+++ Clones/Tiktok/tiktok_clone/lib/features/videos/widgets/video_post.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class VideoPost extends StatefulWidget {
+  const VideoPost({super.key});
+
+  @override
+  State<VideoPost> createState() => _VideoPostState();
+}
+
+class _VideoPostState extends State<VideoPost> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
