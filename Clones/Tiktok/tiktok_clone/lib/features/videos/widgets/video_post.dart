11498206903af9b87fbea83dec621990202c4371
BenKang <rksk1588@gmail.com>
2023-04-02 17:01:27 +0900

    2023.04.02
    Video Player

--- Clones/Tiktok/tiktok_clone/lib/features/videos/widgets/video_post.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/videos/widgets/video_post.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoPost extends StatefulWidget {
-  const VideoPost({super.key});
+  final Function onVideoFinished;
+
+  const VideoPost({
+    super.key,
+    required this.onVideoFinished,
+  });
 
   @override
   State<VideoPost> createState() => _VideoPostState();
@@ -12,16 +17,32 @@ class _VideoPostState extends State<VideoPost> {
   final VideoPlayerController _videoPlayerController =
       VideoPlayerController.asset("assets/videos/video2.mp4");
 
+  void _onVideoChange() {
+    if (_videoPlayerController.value.isInitialized) {
+      if (_videoPlayerController.value.duration ==
+          _videoPlayerController.value.position) {
+        widget.onVideoFinished();
+      }
+    }
+  }
+
   void _initVideoPlayer() async {
     await _videoPlayerController.initialize();
     _videoPlayerController.play();
     setState(() {});
+    _videoPlayerController.addListener(_onVideoChange);
   }
 
   @override
   void initState() {
     super.initState();
     _initVideoPlayer();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
   }
 
   @override
