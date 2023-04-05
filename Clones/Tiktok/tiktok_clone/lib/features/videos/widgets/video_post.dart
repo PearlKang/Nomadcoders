1dac6e1da4d935b479f89727c8bdedbeec2ce6c7
BenKang <rksk1588@gmail.com>
2023-04-06 00:07:52 +0900

    2023.04.06
    AnimatedBuilder

--- Clones/Tiktok/tiktok_clone/lib/features/videos/widgets/video_post.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/videos/widgets/video_post.dart
@@ -105,8 +105,14 @@ class _VideoPostState extends State<VideoPost>
           Positioned.fill(
             child: IgnorePointer(
               child: Center(
-                child: Transform.scale(
-                  scale: _animationController.value,
+                child: AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (context, child) {
+                    return Transform.scale(
+                      scale: _animationController.value,
+                      child: child,
+                    );
+                  },
                   child: AnimatedOpacity(
                     opacity: _isPaused ? 1 : 0,
                     duration: _animationDuration,
