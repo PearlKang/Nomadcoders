3e4d4dd3019c74b7f48eff8a8e78959085ade4e4
BenKang <rksk1588@gmail.com>
2023-04-20 21:21:09 +0900

    2023.04.20

--- Clones/Tiktok/tiktok_clone/lib/features/discover/discover_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/discover/discover_screen.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
 
+final tabs = [
+  "Top",
+  "Users",
+  "Videos",
+  "Sounds",
+  "LIVE",
+  "Shopping",
+  "Brands",
+];
+
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Discover"),
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Discover"),
+          bottom: TabBar(
+            labelStyle: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+            indicatorColor: Colors.black,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey.shade500,
+            tabs: [
+              for (var tab in tabs)
+                Tab(
+                  text: tab,
+                ),
+            ],
+          ),
+        ),
       ),
     );
   }
