996815235e090626d2622d8c3311659afbd88934
BenKang <rksk1588@gmail.com>
2023-04-21 01:03:47 +0900

    2023.04.21
    GridView

--- Clones/Tiktok/tiktok_clone/lib/features/discover/discover_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/discover/discover_screen.dart
@@ -45,7 +45,24 @@ class DiscoverScreen extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
-            Container(),
+            GridView.builder(
+              itemCount: 20,
+              padding: const EdgeInsets.all(
+                Sizes.size6,
+              ),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: Sizes.size10,
+                mainAxisSpacing: Sizes.size10,
+                childAspectRatio: 9 / 16,
+              ),
+              itemBuilder: (context, index) => Container(
+                color: Colors.teal,
+                child: Center(
+                  child: Text("$index"),
+                ),
+              ),
+            ),
             for (var tab in tabs.skip(1))
               Center(
                 child: Text(
