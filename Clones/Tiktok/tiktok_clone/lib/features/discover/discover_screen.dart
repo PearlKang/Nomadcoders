033dd06fb0dfd4610a80fcf33964b07c43f05063
BenKang <rksk1588@gmail.com>
2023-04-22 16:05:21 +0900

    2023.04.22

--- Clones/Tiktok/tiktok_clone/lib/features/discover/discover_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/discover/discover_screen.dart
@@ -56,10 +56,7 @@ class DiscoverScreen extends StatelessWidget {
                 mainAxisSpacing: Sizes.size10,
                 childAspectRatio: 9 / 16,
               ),
-              itemBuilder: (context, index) =>
-                  // Image.asset("assets/images/iu.jpg"),
-                  // Image.network("https://images.unsplash.com/photo-1673844969019-c99b0c933e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"),
-                  FadeInImage.assetNetwork(
+              itemBuilder: (context, index) => FadeInImage.assetNetwork(
                 placeholder: "assets/images/iu.jpg",
                 image:
                     "https://images.unsplash.com/photo-1673844969019-c99b0c933e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
