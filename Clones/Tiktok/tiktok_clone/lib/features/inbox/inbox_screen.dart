3d1a66d57ba3c5f01fdccb93961b48f57bcf8c6a
BenKang <rksk1588@gmail.com>
2023-04-26 00:28:23 +0900

    2023.04.26

--- Clones/Tiktok/tiktok_clone/lib/features/inbox/inbox_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/inbox/inbox_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InboxScreen extends StatelessWidget {
   const InboxScreen({super.key});
 
+  void _onDmPressed() {}
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Inbox"),
+        actions: [
+          IconButton(
+            onPressed: _onDmPressed,
+            icon: const FaIcon(FontAwesomeIcons.paperPlane),
+          ),
+        ],
+      ),
+    );
   }
 }
