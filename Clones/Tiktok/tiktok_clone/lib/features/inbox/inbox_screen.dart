ac431baa60dda39ce077b80800c2614130a688c8
BenKang <rksk1588@gmail.com>
2023-04-26 00:39:51 +0900

    2023.04.26

--- Clones/Tiktok/tiktok_clone/lib/features/inbox/inbox_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/inbox/inbox_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class InboxScreen extends StatelessWidget {
   const InboxScreen({super.key});
@@ -10,11 +11,50 @@ class InboxScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 1,
         title: const Text("Inbox"),
         actions: [
           IconButton(
             onPressed: _onDmPressed,
             icon: const FaIcon(FontAwesomeIcons.paperPlane),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: const [
+          ListTile(
+            title: Text(
+              "Activity",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: Sizes.size16,
+              ),
+            ),
+            trailing: FaIcon(
+              FontAwesomeIcons.chevronRight,
+              size: Sizes.size14,
+              color: Colors.black,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              "New followers",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: Sizes.size16,
+              ),
+            ),
+            subtitle: Text(
+              "Messages from followers will appear here",
+              style: TextStyle(
+                fontSize: Sizes.size14,
+              ),
+            ),
+            trailing: FaIcon(
+              FontAwesomeIcons.chevronRight,
+              size: Sizes.size14,
+              color: Colors.black,
+            ),
           ),
         ],
       ),
