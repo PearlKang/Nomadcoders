d2289df6e7319b28eac07be5fa45fce9f49a64da
BenKang <rksk1588@gmail.com>
2023-04-27 01:28:19 +0900

    2023.04.27

--- Clones/Tiktok/tiktok_clone/lib/features/inbox/activity_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/inbox/activity_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class ActivityScreen extends StatelessWidget {
   const ActivityScreen({super.key});
@@ -8,6 +11,42 @@ class ActivityScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("All activity"),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size20,
+        ),
+        children: [
+          Gaps.v14,
+          Text(
+            "New",
+            style: TextStyle(
+              fontSize: Sizes.size14,
+              color: Colors.grey.shade500,
+            ),
+          ),
+          Gaps.v14,
+          ListTile(
+            contentPadding: EdgeInsets.zero,
+            leading: Container(
+              width: Sizes.size52,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+                border: Border.all(
+                  color: Colors.grey.shade400,
+                  width: Sizes.size1,
+                ),
+              ),
+              child: const Center(
+                child: FaIcon(
+                  FontAwesomeIcons.bell,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
