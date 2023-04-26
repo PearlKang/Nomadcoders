72a85d361b4524f191184b528bc4ac51fe36ede9
BenKang <rksk1588@gmail.com>
2023-04-27 01:16:47 +0900

    2023.04.27

---
+++ Clones/Tiktok/tiktok_clone/lib/features/inbox/activity_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ActivityScreen extends StatelessWidget {
+  const ActivityScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("All activity"),
+      ),
+    );
+  }
+}
--- Clones/Tiktok/tiktok_clone/lib/features/inbox/inbox_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/inbox/inbox_screen.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/inbox/activity_screen.dart';
 
 class InboxScreen extends StatelessWidget {
   const InboxScreen({super.key});
 
   void _onDmPressed() {}
+
+  void _onActivityTap(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ActivityScreen(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +34,7 @@ class InboxScreen extends StatelessWidget {
       body: ListView(
         children: [
           ListTile(
-            onTap: () {},
+            onTap: () => _onActivityTap(context),
             title: const Text(
               "Activity",
               style: TextStyle(
