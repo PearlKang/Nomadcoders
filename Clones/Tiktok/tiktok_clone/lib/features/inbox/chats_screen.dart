c906e329e5fb99da0d8d353049407e69b23cb67f
BenKang <rksk1588@gmail.com>
2023-05-12 22:36:28 +0900

    2023.05.12
    Direct Messages

--- Clones/Tiktok/tiktok_clone/lib/features/inbox/chats_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/inbox/chats_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class ChatsScreen extends StatefulWidget {
@@ -15,28 +16,47 @@ class _ChatsScreenState extends State<ChatsScreen> {
       appBar: AppBar(
         elevation: 1,
         title: const Text("Direct messages"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const FaIcon(
+              FontAwesomeIcons.plus,
+            ),
+          ),
+        ],
       ),
       body: ListView(
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size10,
+        ),
         children: [
           ListTile(
             leading: const CircleAvatar(
               radius: 30,
+              foregroundImage: NetworkImage(
+                  "https://avatars.githubusercontent.com/u/41991469?v=4"),
               child: Text("Ben"),
             ),
-            title: const Text(
-              "bennn",
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-              ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                const Text(
+                  "bennn",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  "2:16 PM",
+                  style: TextStyle(
+                    color: Colors.grey.shade500,
+                    fontSize: Sizes.size12,
+                  ),
+                ),
+              ],
             ),
             subtitle: const Text("Don't forget to make video"),
-            trailing: Text(
-              "2:16 PM",
-              style: TextStyle(
-                color: Colors.grey.shade500,
-                fontSize: Sizes.size12,
-              ),
-            ),
           ),
         ],
       ),
