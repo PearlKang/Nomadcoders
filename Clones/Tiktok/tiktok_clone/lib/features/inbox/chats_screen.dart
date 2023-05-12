75571ceb80090f59b7b27ce03108aad6c467a180
BenKang <rksk1588@gmail.com>
2023-05-12 22:07:25 +0900

    2023.05.12

---
+++ Clones/Tiktok/tiktok_clone/lib/features/inbox/chats_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ChatsScreen extends StatefulWidget {
+  const ChatsScreen({super.key});
+
+  @override
+  State<ChatsScreen> createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
