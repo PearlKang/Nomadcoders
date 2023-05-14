219582dc938075e6103a3251761e54f26265f479
BenKang <rksk1588@gmail.com>
2023-05-14 16:43:27 +0900

    2023.05.14

--- Clones/Tiktok/tiktok_clone/lib/features/inbox/chats_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/inbox/chats_screen.dart
@@ -34,7 +34,7 @@ class _ChatsScreenState extends State<ChatsScreen> {
             leading: const CircleAvatar(
               radius: 30,
               foregroundImage: NetworkImage(
-                "https://avatars.githubusercontent.com/u/41991469?v=4",
+                "https://avatars.githubusercontent.com/u/41991469",
               ),
               child: Text("Ben"),
             ),
