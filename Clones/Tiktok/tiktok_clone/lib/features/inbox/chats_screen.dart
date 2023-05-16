30f7c8ad8e06549dd0d2b36b498e266af337503d
BenKang <rksk1588@gmail.com>
2023-05-17 01:18:41 +0900

    2023.05.17

--- Clones/Tiktok/tiktok_clone/lib/features/inbox/chats_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/inbox/chats_screen.dart
@@ -19,9 +19,7 @@ class _ChatsScreenState extends State<ChatsScreen> {
         actions: [
           IconButton(
             onPressed: () {},
-            icon: const FaIcon(
-              FontAwesomeIcons.plus,
-            ),
+            icon: const FaIcon(FontAwesomeIcons.plus),
           ),
         ],
       ),
