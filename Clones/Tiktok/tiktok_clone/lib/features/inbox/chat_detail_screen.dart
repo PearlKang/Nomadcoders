a9c24db16b4e65c520f8cc49e00009751cce8ee2
BenKang <rksk1588@gmail.com>
2023-06-04 15:51:53 +0900

    2023.06.04
    init

--- Clones/Tiktok/tiktok_clone/lib/features/inbox/chat_detail_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/inbox/chat_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class ChatDetailScreen extends StatefulWidget {
   const ChatDetailScreen({super.key});
@@ -10,6 +13,34 @@ class ChatDetailScreen extends StatefulWidget {
 class _ChatDetailScreenState extends State<ChatDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: ListTile(
+          title: const Text(
+            "Ben",
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          subtitle: const Text("Active now"),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              FaIcon(
+                FontAwesomeIcons.flag,
+                color: Colors.black,
+                size: Sizes.size20,
+              ),
+              Gaps.h32,
+              FaIcon(
+                FontAwesomeIcons.ellipsis,
+                color: Colors.black,
+                size: Sizes.size20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
