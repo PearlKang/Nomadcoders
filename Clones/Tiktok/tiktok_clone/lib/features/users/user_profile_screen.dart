67e5514ab29a9c2fe8f3401ed383bc295ae4d9ec
BenKang <rksk1588@gmail.com>
2023-06-05 20:43:51 +0900

    2023.06.05
    init

---
+++ Clones/Tiktok/tiktok_clone/lib/features/users/user_profile_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class UserProfileScreen extends StatefulWidget {
+  const UserProfileScreen({super.key});
+
+  @override
+  State<UserProfileScreen> createState() => _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends State<UserProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
