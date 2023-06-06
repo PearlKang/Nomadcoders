bbbb2c0a0b552ce93b64720bdd3c5eaef7f81975
BenKang <rksk1588@gmail.com>
2023-06-06 16:47:01 +0900

    2023.06.06

--- Clones/Tiktok/tiktok_clone/lib/features/users/user_profile_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/users/user_profile_screen.dart
@@ -15,11 +15,15 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
         SliverAppBar(
           backgroundColor: Colors.teal,
           elevation: 1,
-          title: const Text("hello!"),
+          title: const Text("1 hello!"),
           collapsedHeight: 80,
           expandedHeight: 200,
           flexibleSpace: FlexibleSpaceBar(
-            background: Image.asset("assets/images/iu.jpg"),
+            background: Image.asset(
+              "assets/images/iu.jpg",
+              fit: BoxFit.cover,
+            ),
+            title: const Text("2 hello!"),
           ),
         ),
       ],
