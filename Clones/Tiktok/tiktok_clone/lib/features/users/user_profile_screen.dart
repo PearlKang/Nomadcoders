6ee529014dc4455f6c3e2f21f1d45218d17135b7
BenKang <rksk1588@gmail.com>
2023-06-10 17:18:05 +0900

    2023.06.10

--- Clones/Tiktok/tiktok_clone/lib/features/users/user_profile_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/users/user_profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({super.key});
@@ -20,9 +22,42 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
               onPressed: () {},
               icon: const FaIcon(
                 FontAwesomeIcons.gear,
+                size: Sizes.size20,
               ),
             ),
           ],
+        ),
+        SliverToBoxAdapter(
+          child: Column(
+            children: [
+              const CircleAvatar(
+                radius: 50,
+                foregroundImage: NetworkImage(
+                  "https://avatars.githubusercontent.com/u/41991469?v=4",
+                ),
+                child: Text("Ben"),
+              ),
+              Gaps.v20,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "@Ben",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: Sizes.size18,
+                    ),
+                  ),
+                  Gaps.h5,
+                  FaIcon(
+                    FontAwesomeIcons.solidCircleCheck,
+                    size: Sizes.size16,
+                    color: Colors.blue.shade500,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ],
     );
