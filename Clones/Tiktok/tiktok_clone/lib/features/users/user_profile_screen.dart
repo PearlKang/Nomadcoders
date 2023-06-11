ce3f9e652c1e9bc055e87d7d8cc7e50edccb4710
BenKang <rksk1588@gmail.com>
2023-06-11 16:06:29 +0900

    2023.06.11

--- Clones/Tiktok/tiktok_clone/lib/features/users/user_profile_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/users/user_profile_screen.dart
@@ -24,120 +24,139 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          title: const Text("Ben"),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const FaIcon(
-                FontAwesomeIcons.gear,
-                size: Sizes.size20,
-              ),
-            ),
-          ],
-        ),
-        SliverToBoxAdapter(
-          child: Column(
-            children: [
-              const CircleAvatar(
-                radius: 50,
-                foregroundImage: NetworkImage(
-                  "https://avatars.githubusercontent.com/u/41991469?v=4",
+    return DefaultTabController(
+      length: 2,
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: const Text("Ben"),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const FaIcon(
+                  FontAwesomeIcons.gear,
+                  size: Sizes.size20,
                 ),
-                child: Text("Ben"),
               ),
-              Gaps.v20,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "@Ben",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: Sizes.size18,
-                    ),
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                const CircleAvatar(
+                  radius: 50,
+                  foregroundImage: NetworkImage(
+                    "https://avatars.githubusercontent.com/u/41991469?v=4",
                   ),
-                  Gaps.h5,
-                  FaIcon(
-                    FontAwesomeIcons.solidCircleCheck,
-                    size: Sizes.size16,
-                    color: Colors.blue.shade500,
-                  ),
-                ],
-              ),
-              Gaps.v24,
-              SizedBox(
-                height: Sizes.size48,
-                child: Row(
+                  child: Text("Ben"),
+                ),
+                Gaps.v20,
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    const UserAccount(count: "97", text: "Following"),
-                    _verticalDivider(),
-                    const UserAccount(count: "10M", text: "Followers"),
-                    _verticalDivider(),
-                    const UserAccount(count: "194.3M", text: "Likes"),
-                  ],
-                ),
-              ),
-              Gaps.v14,
-              FractionallySizedBox(
-                widthFactor: 0.33,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: Sizes.size12,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(
-                        Sizes.size4,
+                    const Text(
+                      "@Ben",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: Sizes.size18,
                       ),
                     ),
-                  ),
-                  child: const Text(
-                    "Follow",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
+                    Gaps.h5,
+                    FaIcon(
+                      FontAwesomeIcons.solidCircleCheck,
+                      size: Sizes.size16,
+                      color: Colors.blue.shade500,
                     ),
+                  ],
+                ),
+                Gaps.v24,
+                SizedBox(
+                  height: Sizes.size48,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const UserAccount(count: "97", text: "Following"),
+                      _verticalDivider(),
+                      const UserAccount(count: "10M", text: "Followers"),
+                      _verticalDivider(),
+                      const UserAccount(count: "194.3M", text: "Likes"),
+                    ],
+                  ),
+                ),
+                Gaps.v14,
+                FractionallySizedBox(
+                  widthFactor: 0.33,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: Sizes.size12,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(
+                          Sizes.size4,
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      "Follow",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Gaps.v14,
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: Sizes.size32,
+                  ),
+                  child: Text(
+                    "All highlights and where to watch live matches on FIFA+ I wonder how it would loook",
                     textAlign: TextAlign.center,
                   ),
                 ),
-              ),
-              Gaps.v14,
-              const Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: Sizes.size32,
-                ),
-                child: Text(
-                  "All highlights and where to watch live matches on FIFA+ I wonder how it would loook",
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Gaps.v14,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  FaIcon(
-                    FontAwesomeIcons.link,
-                    size: Sizes.size12,
-                  ),
-                  Gaps.h4,
-                  Text(
-                    "https://www.github.com/pearlkang",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
+                Gaps.v14,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    FaIcon(
+                      FontAwesomeIcons.link,
+                      size: Sizes.size12,
                     ),
+                    Gaps.h4,
+                    Text(
+                      "https://www.github.com/pearlkang",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                Gaps.v5,
+                const TabBar(
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicatorColor: Colors.black,
+                  labelPadding: EdgeInsets.only(
+                    bottom: Sizes.size10,
                   ),
-                ],
-              ),
-              Gaps.v5,
-            ],
+                  labelColor: Colors.black,
+                  tabs: [
+                    Icon(
+                      Icons.grid_4x4_rounded,
+                    ),
+                    FaIcon(
+                      FontAwesomeIcons.heart,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
