51dece8d08af2cd6c002c5d14e7ccffb6c054bad
BenKang <rksk1588@gmail.com>
2023-06-11 16:19:43 +0900

    2023.06.11
    TabBar

--- Clones/Tiktok/tiktok_clone/lib/features/users/user_profile_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/users/user_profile_screen.dart
@@ -175,12 +175,88 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                 SizedBox(
                   width: MediaQuery.of(context).size.width,
                   height: MediaQuery.of(context).size.width,
-                  child: const TabBarView(
+                  child: TabBarView(
                     children: [
-                      Center(
-                        child: Text("page one"),
+                      GridView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        keyboardDismissBehavior:
+                            ScrollViewKeyboardDismissBehavior.onDrag,
+                        itemCount: 20,
+                        padding: const EdgeInsets.all(
+                          Sizes.size6,
+                        ),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: Sizes.size10,
+                          mainAxisSpacing: Sizes.size10,
+                          childAspectRatio: 9 / 20,
+                        ),
+                        itemBuilder: (context, index) => Column(
+                          children: [
+                            Container(
+                              clipBehavior: Clip.hardEdge,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.circular(Sizes.size4),
+                              ),
+                              child: AspectRatio(
+                                aspectRatio: 9 / 16,
+                                child: FadeInImage.assetNetwork(
+                                  fit: BoxFit.cover,
+                                  placeholderFit: BoxFit.cover,
+                                  placeholder: "assets/images/iu.jpg",
+                                  image:
+                                      "https://images.unsplash.com/photo-1673844969019-c99b0c933e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
+                                ),
+                              ),
+                            ),
+                            Gaps.v10,
+                            const Text(
+                              "This is a very long caption for my tiktok that im upload just now currently.",
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                              style: TextStyle(
+                                fontSize: Sizes.size16 + Sizes.size2,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Gaps.v8,
+                            DefaultTextStyle(
+                              style: TextStyle(
+                                color: Colors.grey.shade600,
+                                fontWeight: FontWeight.w600,
+                              ),
+                              child: Row(
+                                children: [
+                                  const CircleAvatar(
+                                    radius: 12,
+                                    backgroundImage: NetworkImage(
+                                        "https://avatars.githubusercontent.com/u/41991469?v=4"),
+                                  ),
+                                  Gaps.h4,
+                                  const Expanded(
+                                    child: Text(
+                                      "My avatar is going to be very long",
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  Gaps.h4,
+                                  FaIcon(
+                                    FontAwesomeIcons.heart,
+                                    size: Sizes.size16,
+                                    color: Colors.grey.shade600,
+                                  ),
+                                  Gaps.h2,
+                                  const Text("2.5M"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      Center(
+                      const Center(
                         child: Text("page two"),
                       ),
                     ],
