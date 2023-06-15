3c7ea24415543030fd9fbe5e241173ce8460b431
BenKang <rksk1588@gmail.com>
2023-06-15 21:30:40 +0900

    2023.06.15

--- Clones/Tiktok/tiktok_clone/lib/features/settings/settings_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/settings/settings_screen.dart
@@ -9,9 +9,26 @@ class SettingScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Settings"),
       ),
-      body: Column(
-        children: const [
-          CloseButton(),
+      body: ListWheelScrollView(
+        useMagnifier: true,
+        magnification: 1.5,
+        itemExtent: 200,
+        children: [
+          for (var i in [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: Container(
+                color: Colors.teal,
+                alignment: Alignment.center,
+                child: const Text(
+                  "Pick me",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 39,
+                  ),
+                ),
+              ),
+            ),
         ],
       ),
     );
