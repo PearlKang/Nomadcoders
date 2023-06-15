3a3a3c4e1f78f785c47f4a518b191fae67eebcba
BenKang <rksk1588@gmail.com>
2023-06-15 21:34:18 +0900

    2023.06.15

--- Clones/Tiktok/tiktok_clone/lib/features/settings/settings_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/settings/settings_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SettingScreen extends StatelessWidget {
@@ -9,26 +10,10 @@ class SettingScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Settings"),
       ),
-      body: ListWheelScrollView(
-        diameterRatio: 1.5,
-        offAxisFraction: 1.5,
-        itemExtent: 200,
-        children: [
-          for (var i in [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: Container(
-                color: Colors.teal,
-                alignment: Alignment.center,
-                child: const Text(
-                  "Pick me",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 39,
-                  ),
-                ),
-              ),
-            ),
+      body: Column(
+        children: const [
+          CupertinoActivityIndicator(),
+          CircularProgressIndicator(),
         ],
       ),
     );
