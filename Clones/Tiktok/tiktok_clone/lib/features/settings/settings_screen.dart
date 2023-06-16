90e7d3a7d27dff679a0ccf4845a9451d452a0b4d
BenKang <rksk1588@gmail.com>
2023-06-16 20:41:44 +0900

    2023.06.16
    AboutListTile

--- Clones/Tiktok/tiktok_clone/lib/features/settings/settings_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/settings/settings_screen.dart
@@ -10,22 +10,8 @@ class SettingScreen extends StatelessWidget {
         title: const Text("Settings"),
       ),
       body: ListView(
-        children: [
-          ListTile(
-            onTap: () => showAboutDialog(
-              context: context,
-              applicationVersion: "1.0",
-              applicationLegalese: "All rights reseverd. Please dont copy me.",
-            ),
-            title: const Text(
-              "About",
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            subtitle: const Text("About this app...."),
-          ),
-          const AboutListTile(),
+        children: const [
+          AboutListTile(),
         ],
       ),
     );
