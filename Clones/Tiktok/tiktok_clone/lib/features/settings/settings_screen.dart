6e9836a61f769cc16e98cc983e578e895eb5ed14
BenKang <rksk1588@gmail.com>
2023-06-17 21:19:27 +0900

    2023.06.17
    showDateRangePicker

--- Clones/Tiktok/tiktok_clone/lib/features/settings/settings_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/settings/settings_screen.dart
@@ -20,6 +20,30 @@ class SettingScreen extends StatelessWidget {
                 lastDate: DateTime(2030),
               );
               print(date);
+
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              print(time);
+
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                    data: ThemeData(
+                      appBarTheme: const AppBarTheme(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black,
+                      ),
+                    ),
+                    child: child!,
+                  );
+                },
+              );
+              print(booking);
             },
             title: const Text("What is your birthday?"),
           ),
