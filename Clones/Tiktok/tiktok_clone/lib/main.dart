9ea2941ac489c2c2f1b643acf2d83f201c6e2c03
BenKang <rksk1588@gmail.com>
2023-06-22 21:15:28 +0900

    2023.06.22
    OrientationBuilder

--- Clones/Tiktok/tiktok_clone/lib/main.dart
+++ Clones/Tiktok/tiktok_clone/lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+    ],
+  );
+
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle.dark,
+  );
+
   runApp(const TikTokApp());
 }
 
