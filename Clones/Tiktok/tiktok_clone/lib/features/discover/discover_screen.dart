eae66da5050a53de9f1db9fa31660e13b45dfece
BenKang <rksk1588@gmail.com>
2023-06-28 21:21:49 +0900

    2023.06.28
    MediaQuery

--- Clones/Tiktok/tiktok_clone/lib/features/discover/discover_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/discover/discover_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/breakpoints.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
