ec1eb0e1f676c861500fac00111221a5d2ddeb66
BenKang <rksk1588@gmail.com>
2023-07-01 16:20:14 +0900

    2023.07.01
    init

--- Clones/Tiktok/tiktok_clone/lib/features/discover/discover_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/discover/discover_screen.dart
@@ -51,10 +51,15 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
           elevation: 1,
-          title: CupertinoSearchTextField(
-            controller: _textEditingController,
-            onChanged: _onSearchChanged,
-            onSubmitted: _onSearchSubmitted,
+          title: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: Breakpoints.sm,
+            ),
+            child: CupertinoSearchTextField(
+              controller: _textEditingController,
+              onChanged: _onSearchChanged,
+              onSubmitted: _onSearchSubmitted,
+            ),
           ),
           bottom: TabBar(
             onTap: (value) => _onTapChanged(),
