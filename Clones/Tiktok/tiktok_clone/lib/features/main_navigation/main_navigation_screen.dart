3e57057370b38d45eb3615fea7d1aac05debae09
BenKang <rksk1588@gmail.com>
2023-07-10 21:24:42 +0900

    2023.07.10
    init

--- Clones/Tiktok/tiktok_clone/lib/features/main_navigation/main_navigation_screen.dart
+++ Clones/Tiktok/tiktok_clone/lib/features/main_navigation/main_navigation_screen.dart
@@ -45,7 +45,8 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      backgroundColor: _selectedIndex == 0 ? Colors.black : Colors.white,
+      backgroundColor:
+          _selectedIndex == 0 || isDark ? Colors.black : Colors.white,
       body: Stack(
         children: [
           Offstage(
