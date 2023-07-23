347766112ddb5bb271193cc66b93de4b3b1f1f2f
BenKang <rksk1588@gmail.com>
2023-07-23 21:15:09 +0900

    2023.07.23
    init

--- Clones/Tiktok/tiktok_clone/lib/main.dart
+++ Clones/Tiktok/tiktok_clone/lib/main.dart
@@ -28,6 +28,8 @@ class TikTokApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    S.load(const Locale("en"));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
