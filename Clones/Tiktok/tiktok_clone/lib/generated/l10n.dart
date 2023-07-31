4b336998d43cfc6d114626682767e8e6aa178f44
BenKang <rksk1588@gmail.com>
2023-08-01 00:42:43 +0900

    2023.08.01

--- Clones/Tiktok/tiktok_clone/lib/generated/l10n.dart
+++ Clones/Tiktok/tiktok_clone/lib/generated/l10n.dart
@@ -50,13 +50,13 @@ class S {
     return Localizations.of<S>(context, S);
   }
 
-  /// `Sign up for {nameOfTheApp}`
-  String signUpTitle(String nameOfTheApp) {
+  /// `Sign up for {nameOfTheApp} {when}`
+  String signUpTitle(String nameOfTheApp, DateTime when) {
     return Intl.message(
-      'Sign up for $nameOfTheApp',
+      'Sign up for $nameOfTheApp $when',
       name: 'signUpTitle',
       desc: 'The title people see when they open the app for the first time.',
-      args: [nameOfTheApp],
+      args: [nameOfTheApp, when],
     );
   }
 
