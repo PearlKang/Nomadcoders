5fb5bc26692e99a50f7b6b3ffcbc43c6d2a76c82
BenKang <rksk1588@gmail.com>
2023-08-15 14:36:11 +0900

    2023.08.15
    init

--- Clones/Tiktok/tiktok_clone/lib/router.dart
+++ Clones/Tiktok/tiktok_clone/lib/router.dart
@@ -27,7 +27,11 @@ final router = GoRouter(
       path: "/users/:username",
       builder: (context, state) {
         final username = state.params["params"];
-        return UserProfileScreen(username: username!);
+        final tab = state.queryParams["show"];
+        return UserProfileScreen(
+          username: username!,
+          tab: tab!,
+        );
       },
     ),
   ],
