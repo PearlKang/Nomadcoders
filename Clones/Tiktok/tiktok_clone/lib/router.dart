99441072e8c421920b64679ee5e698dfcfb45f74
BenKang <rksk1588@gmail.com>
2023-08-25 00:06:56 +0900

    2023.08.25
    init

--- Clones/Tiktok/tiktok_clone/lib/router.dart
+++ Clones/Tiktok/tiktok_clone/lib/router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tiktok_clone/features/authentication/email_screen.dart';
 import 'package:tiktok_clone/features/authentication/login_screen.dart';
@@ -17,7 +18,20 @@ final router = GoRouter(
     ),
     GoRoute(
       path: UsernameScreen.routeName,
-      builder: (context, state) => const UsernameScreen(),
+      pageBuilder: (context, state) {
+        return CustomTransitionPage(
+          child: const UsernameScreen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: ScaleTransition(
+                scale: animation,
+                child: child,
+              ),
+            );
+          },
+        );
+      },
     ),
     GoRoute(
       path: EmailScreen.routeName,
