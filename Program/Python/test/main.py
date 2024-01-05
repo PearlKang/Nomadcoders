ad7667c3a7c047930721369f4b048209acfdfaab
BenKang <rksk1588@gmail.com>
2024-01-05 19:30:35 +0900

    2024.01.05
    #2.1 Variables

--- Program/Python/test/main.py
+++ Program/Python/test/main.py
@@ -1,1 +1,16 @@
+#2.0 Hello world
 print("Hello world!")
+
+#2.1 Variables
+a = 2
+b = 5
+c = a + b
+print(c)
+
+my_age = 88 #snake case, 보통 파이썬에서 자주 사용
+myAge = 88  #camel case, 보통 javascript에서 사용
+#myage = 88
+#123my_age = 88
+my_age123 = 88
+#*/my_age = 88
+#my age = 88
