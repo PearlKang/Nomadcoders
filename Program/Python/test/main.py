db4432cec89383e4c23743523849c5560c422d7e
BenKang <rksk1588@gmail.com>
2024-01-07 16:27:33 +0900

    2024.01.07
    #2.8 Recap

--- Program/Python/test/main.py
+++ Program/Python/test/main.py
@@ -6,7 +6,6 @@
 # b = 5
 # c = a + b
 # print(c)
-
 # my_age = 88 #snake case, 보통 파이썬에서 자주 사용
 # myAge = 88  #camel case, 보통 javascript에서 사용
 # myage = 88
@@ -18,14 +17,12 @@
 #2.2 Booleans and Strings
 # my_name = "Ben"
 # print(my_name)
-
 # dead = False # True
 
 #2.3 Recap
 # my_name = "Ben"
 # age = 12
 # dead = False
-
 # print("Hello my name is", my_name)
 # print("and I'm", age, "years old")
 
@@ -34,34 +31,34 @@
 # print("hello")
 # print(12)
 # print(True, "hello", 12)
-
 # def say_hello():
 #     print("hello how r u?")
-
 # say_hello()
 
 #2.5 Indentation
 # def say_hello():
 #     print("hello how r u?")
-
 # def say_bye():
 #     print("bye bye")
-
 # say_hello()
 
 #2.6 Parameters
 # def say_hello(user_name):
 #     print("hello", user_name, "how are you?")
-
 # say_hello("ben")
 # say_hello("ben2")
 # say_hello("ben3")
 
 #2.7 Multiple Parameters
-def say_hello(user_name, user_age):
-    print("hello", user_name)
-    print("you are", user_age, "years old")
+# def say_hello(user_name, user_age):
+#     print("hello", user_name)
+#     print("you are", user_age, "years old")
+# say_hello("ben", 35)
 
-say_hello("ben", 35)
+#2.8 Recap
+def tax_calculator(money):   #salary, bank account, balance
+    print(money * 0.35)
 
+tax_calculator(1500000000000)
+tax_calculator(150)
 
