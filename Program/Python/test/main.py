56363d8172ae6af5232ce257ca7c38de0637369e
BenKang <rksk1588@gmail.com>
2024-01-07 16:06:46 +0900

    2024.01.07
    #2.4 Functions

--- Program/Python/test/main.py
+++ Program/Python/test/main.py
@@ -1,30 +1,41 @@
 #2.0 Hello world
-print("Hello world!")
+# print("Hello world!")
 
 #2.1 Variables
-a = 2
-b = 5
-c = a + b
-print(c)
+# a = 2
+# b = 5
+# c = a + b
+# print(c)
 
-my_age = 88 #snake case, 보통 파이썬에서 자주 사용
-myAge = 88  #camel case, 보통 javascript에서 사용
-#myage = 88
-#123my_age = 88
-my_age123 = 88
-#*/my_age = 88
-#my age = 88
+# my_age = 88 #snake case, 보통 파이썬에서 자주 사용
+# myAge = 88  #camel case, 보통 javascript에서 사용
+# myage = 88
+# 123my_age = 88
+# my_age123 = 88
+# */my_age = 88
+# my age = 88
 
 #2.2 Booleans and Strings
-my_name = "Ben"
-print(my_name)
+# my_name = "Ben"
+# print(my_name)
 
-dead = False # True
+# dead = False # True
 
 #2.3 Recap
-my_name = "Ben"
-age = 12
-dead = False
+# my_name = "Ben"
+# age = 12
+# dead = False
 
-print("Hello my name is", my_name)
-print("and I'm", age, "years old")
+# print("Hello my name is", my_name)
+# print("and I'm", age, "years old")
+
+#2.4 Functions
+# print(True)
+# print("hello")
+# print(12)
+# print(True, "hello", 12)
+
+def say_hello():
+    print("hello how r u?")
+
+say_hello()
