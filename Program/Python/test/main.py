7a57e02c9f3e6b227d398c2f3de42e52450861ec
BenKang <rksk1588@gmail.com>
2024-01-08 22:20:59 +0900

    2024.01.08
    #2.9 Default Parameters

--- Program/Python/test/main.py
+++ Program/Python/test/main.py
@@ -56,9 +56,26 @@
 # say_hello("ben", 35)
 
 #2.8 Recap
-def tax_calculator(money):   #salary, bank account, balance
-    print(money * 0.35)
+# def tax_calculator(money):   #salary, bank account, balance
+#     print(money * 0.35)
+# tax_calculator(1500000000000)
+# tax_calculator(150)
 
-tax_calculator(1500000000000)
-tax_calculator(150)
+#2.9 Default Parameters
+def say_hello(user_name="anonymous"):
+    print("hello", user_name)
+say_hello("Ben")
+say_hello()
+def plus(a=0, b=0):
+    print(a + b)
+def minus(a=0, b=0):
+    print(a - b)
+def multiply(a=0, b=0):
+    print(a * b)
+def divide(a=0, b=1):
+    print(a / b)
+def power_of(a=0, b=0):
+    print(a ** b)
+
+
 
