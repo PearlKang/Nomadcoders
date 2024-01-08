16af0be29bee67e73ba74102b604f18b942bb895
BenKang <rksk1588@gmail.com>
2024-01-09 00:03:48 +0900

    2024.01.09
    #2.11 Reutrn Recap

--- Program/Python/test/main.py
+++ Program/Python/test/main.py
@@ -82,11 +82,27 @@
 #     print(money * 0.35)
 # def pay_tax(tax):
 #     print("thank you for paying", tax)
-def tax_calc(money):
-    return money * 0.35
-def pay_tax(tax):
-    print("thank you for paying", tax)
-to_pay = tax_calc(150000000)
-pay_tax(to_pay)
-pay_tax(tax_calc(150000000))
+# def tax_calc(money):
+#     return money * 0.35
+# def pay_tax(tax):
+#     print("thank you for paying", tax)
+# to_pay = tax_calc(150000000)
+# pay_tax(to_pay)
+# pay_tax(tax_calc(150000000))
+
+#2.11 Reutrn Recap
+my_name = "ben"
+my_age = 35
+my_color_eyes = "brown"
+print(f"Hello I'm {my_name}, I have {my_age} years in the earth, {my_color_eyes} is my eye color")
+def make_juice(fruit):
+    return f"{fruit}+🥤"
+def add_ice(juice):
+    return f"{juice}+🧊"
+def add_sugar(iced_juice):
+    return f"{iced_juice}+🍬"
+juice = make_juice("🍎")
+cold_juice = add_ice(juice)
+perfect_juice = add_sugar(cold_juice)
+print(perfect_juice)
 
