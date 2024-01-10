496eb344d6ca39d2ee631827b9cb8b4570c94d25
BenKang <rksk1588@gmail.com>
2024-01-10 16:18:52 +0900

    2024.01.10
    #3.1 Else & Elif

--- Program/Python/test/main.py
+++ Program/Python/test/main.py
@@ -107,8 +107,24 @@
 # print(perfect_juice)
 
 #3.0 If
-if 10 > 5:
-    print("Corrent!")
-a = 10
-if a == 10:
-    print("True!")
+# if 10 > 5:
+#     print("Corrent!")
+# a = 10
+# if a == 10:
+#     print("True!")
+
+#3.1 Else & Elif
+password_correct = True
+if password_correct:
+    print("Here is your money")
+else:
+    print("Wrong password")
+winner = 10
+if winner > 10:
+    print("Winner is greater than 10")
+elif winner < 10:
+    print("Winner is less than 10")
+else:
+    print("Winner is 10")
+
+
