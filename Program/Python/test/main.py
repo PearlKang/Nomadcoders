31bdb06c2ffc613df55a83d80a84e310050ef019
BenKang <rksk1588@gmail.com>
2024-01-12 17:55:24 +0900

    2024.01.12
    #3.7 Recap

--- Program/Python/test/main.py
+++ Program/Python/test/main.py
@@ -184,6 +184,22 @@
 #     distance = distance + 1
 
 #3.6 Python Casino
+# from random import randint
+# print("Welcome to Python Casino")
+# pc_choice = randint(1, 100)
+# playing = True
+# while playing:
+#     user_choice = int(input("choose number (1-100): "))
+#     if user_choice == pc_choice:
+#         print("You won!")
+#         playing = False
+#     elif user_choice > pc_choice:
+#         print("Lower!")
+#     elif user_choice < pc_choice:
+#         print("Higher!")
+
+#3.7 Recap
+"""
 from random import randint
 print("Welcome to Python Casino")
 pc_choice = randint(1, 100)
@@ -197,5 +213,6 @@ while playing:
         print("Lower!")
     elif user_choice < pc_choice:
         print("Higher!")
+"""
 
 
