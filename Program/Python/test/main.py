cbd537e9c6fe8a818bc5fadf58b129cd468608d2
BenKang <rksk1588@gmail.com>
2024-01-25 20:14:06 +0900

    2024.01.25
    #5.6 Recap

--- Program/Python/test/main.py
+++ Program/Python/test/main.py
@@ -539,6 +539,7 @@ bibi = Puppy(
 """
 
 #5.5 Inheritance part Two
+"""
 class Dog:
     def __init__(self, name, breed, age):
         self.name = name
@@ -570,7 +571,36 @@ ruffus.woof_woof()
 ruffus.sleep()
 bibi.rrrrr()
 bibi.sleep()
+"""
 
+#5.6 Recap
+class Dog:
+    def __init__(self, name, breed, age):
+        self.name = name
+        self.breed = breed
+        self.age = age
+    def sleep(self):
+        print("zzzzzzzzz.....")
+class GuardDog(Dog):
+    def __init__(self, name, breed):
+        super().__init__(name, breed, 5)
+        self.aggresive = True
+    def rrrrr(self):
+        print("stay away!")
+class Puppy(Dog):
+    def __init__(self, name, breed):
+        super().__init__(name, breed, 0.1)
+        self.spoiled = True
+    def woof_woof(self):
+        print("Woof Woof!")
+ruffus = Puppy(
+    name="Ruffus",
+    breed="Beagle",
+)
+bibi = GuardDog(
+    name="Bibi",
+    breed="Dalmatian",
+)
 
 
 
