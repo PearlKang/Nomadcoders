5d4398c21862e48e388cc778ceace97fa6283c6e
BenKang <rksk1588@gmail.com>
2024-01-30 00:27:04 +0900

    2024.01.30
    #6.3 Jobs

--- Program/Python/test/main.py
+++ Program/Python/test/main.py
@@ -679,5 +679,30 @@ print(jobs)
 """
 
 #6.3 Jobs
+import requests
+from bs4 import BeautifulSoup
+url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+# print(jobs)
+for job in jobs:
+    title = job.find("span", class_="title").text
+    # _region = job.find("span", class_="region").text
+    # print(title, "-------------", region)
+    # companies = job.find_all("span", class_="company")
+    # company, position, _ = job.find_all("span", class_="company")
+    company, position, region = job.find_all("span", class_="company")
+    company = company.text
+    position = position.text
+    region = region.text
+    print(title, company, position, region, "---------\n")
+
+
+
+
+
+
+
 
 
