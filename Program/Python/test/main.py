551eb0998bdd2b7315da30692368010f996bcec7
BenKang <rksk1588@gmail.com>
2024-01-31 00:10:05 +0900

    2024.01.31
    #6.4 Recap

--- Program/Python/test/main.py
+++ Program/Python/test/main.py
@@ -679,6 +679,7 @@ print(jobs)
 """
 
 #6.3 Jobs
+"""
 import requests
 from bs4 import BeautifulSoup
 url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
@@ -697,6 +698,33 @@ for job in jobs:
     position = position.text
     region = region.text
     print(title, company, position, region, "---------\n")
+"""
+
+#6.4 Recap
+import requests
+from bs4 import BeautifulSoup
+url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+all_jobs = []
+for job in jobs:
+    title = job.find("span", class_="title").text
+    company, position, region = job.find_all("span", class_="company")
+    # url = job.find("a")["href"]
+    # url = job.find("a")
+    # if url:
+    #     url = url["href"]
+    url = job.find("div", class_="tooltip").next_sibling["href"]
+    job_data = {
+        "title": title,
+        "company": company.text,
+        "position": position.text,
+        "region": region.text,
+        "url": f"https://weworkremotely.com/{url}"
+    }
+    all_jobs.append(job_data)
+print(all_jobs)
 
 
 
