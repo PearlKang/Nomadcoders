b2fced8d0ba0ef8187dad9102ae5d7fa8562fdd4
BenKang <rksk1588@gmail.com>
2024-02-01 00:32:13 +0900

    2024.02.01
    #6.5 Pagination

--- Program/Python/test/main.py
+++ Program/Python/test/main.py
@@ -701,6 +701,7 @@ for job in jobs:
 """
 
 #6.4 Recap
+"""
 import requests
 from bs4 import BeautifulSoup
 url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
@@ -725,11 +726,43 @@ for job in jobs:
     }
     all_jobs.append(job_data)
 print(all_jobs)
+"""
 
-
-
-
-
+#6.5 Pagination
+import requests
+from bs4 import BeautifulSoup
+# url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
+all_jobs = []
+def scrape_page(url):
+    print(f"Scrapping {url}...")
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+    for job in jobs:
+        title = job.find("span", class_="title").text
+        company, position, region = job.find_all("span", class_="company")
+        url = job.find("div", class_="tooltip").next_sibling["href"]
+        job_data = {
+            "title": title,
+            "company": company.text,
+            "position": position.text,
+            "region": region.text,
+            "url": f"https://weworkremotely.com/{url}"
+        }
+        all_jobs.append(job_data)
+def get_pages(url):
+    # response = requests.get("https://weworkremotely.com/remote-full-time-jobs?page=1")
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    return len(soup.find("div", class_="pagination").find_all("span", class_="page"))
+# print(buttons)
+total_pages = get_pages("https://weworkremotely.com/remote-full-time-jobs?page=1")
+for x in range(total_pages):
+    url = f"https://weworkremotely.com/remote-full-time-jobs?page={x+1}"
+    # print("request page", x+1)
+    # print(url)
+    scrape_page(url)
+print(len(all_jobs))
 
 
 
