44dfc84f9daeeb4118f2561e4e82bd4be4dd43dd
BenKang <rksk1588@gmail.com>
2024-02-22 00:06:32 +0900

    2024.02.21
    #7.3 Form

--- Program/Python/Flask/main.py
+++ Program/Python/Flask/main.py
@@ -7,8 +7,8 @@ def home():
     # return "<h1>hey there!</h1><a href='/hello'>go to hello</a>"
     return render_template("home.html", name="ben")
 
-@app.route("/hello")
+@app.route("/search")
 def hello():
-    return "hello you!"
+    return render_template("search.html")
 
 app.run("127.0.0.1", port=8080, debug=True)
