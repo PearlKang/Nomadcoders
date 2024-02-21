f96bd952e02577c1e5b63c585cb5c309660c0c3c
BenKang <rksk1588@gmail.com>
2024-02-21 18:54:53 +0900

    2024.02.21

--- Program/Python/test/test.py
+++ Program/Python/test/test.py
@@ -1294,3 +1294,37 @@ app.run("127.0.0.1", port=8080, debug=True)
 """
 
 #7.2 Render Template
+"""
+#main.py
+from flask import Flask, render_template
+
+app = Flask("JobScrapper")
+
+@app.route("/")
+def home():
+    # return "<h1>hey there!</h1><a href='/hello'>go to hello</a>"
+    return render_template("home.html", name="ben")
+
+@app.route("/hello")
+def hello():
+    return "hello you!"
+
+app.run("127.0.0.1", port=8080, debug=True)
+"""
+"""
+#home.html
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Job Scrapper</title>
+  </head>
+  <body>
+    <h1>Hello to you! My name is {{name}}</h1>
+  </body>
+</html>
+"""
+
+#7.3 Form
