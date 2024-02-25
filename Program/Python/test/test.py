2fed72aeb1efe0a05fe25ed107d6d57f204cbaa3
BenKang <rksk1588@gmail.com>
2024-02-25 15:13:31 +0900

    2024.02.25

--- Program/Python/test/test.py
+++ Program/Python/test/test.py
@@ -1416,3 +1416,48 @@ app.run("127.0.0.1", port=8080, debug=True)
 """
 
 #7.5 Arguments
+"""
+from flask import Flask, render_template, request
+import csv
+
+# old version
+# from extractors.indeed import exxtract_indeed_jobs
+# from extractors.wwr import extract_wwr_jobs
+
+app = Flask("JobScrapper")
+
+@app.route("/")
+def home():
+    return render_template("home.html", name="ben")
+
+@app.route("/search")
+def hello():
+    # print(request.args)
+    keyword = request.args.get("keyword")
+
+    # old version
+    # indeed = extract_indeed_jobs(keyword)
+    # wwr = extract_wwr_jobs(keyword)
+    # jobs = indeed + wwr
+    # return render_template("search.html", keyword=keyword, jobs=jobs)
+
+    return render_template("search.html", keyword=keyword)
+
+app.run("127.0.0.1", port=8080, debug=True)
+"""
+"""
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Job Scrapper</title>
+  </head>
+  <body>
+    <h1>Search Results for "{{keyword}}":</h1>
+  </body>
+</html>
+"""
+
+#7.6 For Loops
