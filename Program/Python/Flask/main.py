c96bb80ed18f184003af0967d071f08d8e7ff04b
BenKang <rksk1588@gmail.com>
2024-02-25 15:12:46 +0900

    2024.02.25
    #7.5 Arguments

--- Program/Python/Flask/main.py
+++ Program/Python/Flask/main.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import csv
+
+# old version
+# from extractors.indeed import exxtract_indeed_jobs
+# from extractors.wwr import extract_wwr_jobs
 
 app = Flask("JobScrapper")
 
@@ -8,6 +13,15 @@ def home():
 
 @app.route("/search")
 def hello():
-    return render_template("search.html")
+    # print(request.args)
+    keyword = request.args.get("keyword")
+
+    # old version
+    # indeed = extract_indeed_jobs(keyword)
+    # wwr = extract_wwr_jobs(keyword)
+    # jobs = indeed + wwr
+    # return render_template("search.html", keyword=keyword, jobs=jobs)
+
+    return render_template("search.html", keyword=keyword)
 
 app.run("127.0.0.1", port=8080, debug=True)
