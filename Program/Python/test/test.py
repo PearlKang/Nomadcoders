b44b99168f4f18baa35d8ba18471fc48e6888d23
BenKang <rksk1588@gmail.com>
2024-03-01 17:00:40 +0900

    2024.03.01

--- Program/Python/test/test.py
+++ Program/Python/test/test.py
@@ -1666,3 +1666,32 @@ class JobScraper():
 """
 
 #7.8 Cache
+"""from flask import Flask, render_template, request
+import jobscrapper
+import csv
+
+app = Flask("JobScrapper")
+
+db = {}
+
+@app.route("/")
+def home():
+    return render_template("home.html", name="ben")
+
+@app.route("/search")
+def hello():
+    keyword = request.args.get("keyword")
+
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        job_scraper = jobscrapper.JobScraper(keyword)
+        jobs = job_scraper.run_app()
+        db[keyword] = jobs
+
+    return render_template("search.html", keyword=keyword, jobs=jobs)
+
+app.run("127.0.0.1", port=8080, debug=True)
+"""
+
+#7.9 File Download
