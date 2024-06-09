119b63ec105b7c4b14483c720e858734db8328f1
BenKang <rksk1588@gmail.com>
2024-06-10 00:49:39 +0900

    2024.06.10
    #7.2 Data Flow

--- Program/GPT/fullstack-gpt/Home.py
+++ Program/GPT/fullstack-gpt/Home.py
@@ -1,20 +1,35 @@
 import streamlit as st
-from langchain.prompts import PromptTemplate
+from datetime import datetime
 
 
-st.write("hello")
+today = datetime.today().strftime("%H:%M:%S")
 
-a = [1, 2, 3, 4]
+st.title(today)
 
-d = {"x": 1}
 
-p = PromptTemplate.from_template("xxxx")
-
-a
-
-d
-
-st.selectbox(
+model = st.selectbox(
     "Choose your model",
-    ("GPT-3", "GPT-4"),
+    (
+        "GPT-3",
+        "GPT-4",
+    ),
 )
+
+# st.write(model)
+
+if model == "GPT-3":
+    st.write("cheap")
+else:
+    st.write("not cheap")
+
+    name = st.text_input("What is your name?")
+
+    st.write(name)
+
+    value = st.slider(
+        "temperature",
+        min_value=0.1,
+        max_value=1.0,
+    )
+
+    st.write(value)
