81f4862bb0286d5009e3f6627def5b73091c9c52
BenKang <rksk1588@gmail.com>
2024-06-12 01:06:59 +0900

    2024.06.12
    #7.3 Multi Page

--- Program/GPT/fullstack-gpt/Home.py
+++ Program/GPT/fullstack-gpt/Home.py
@@ -1,21 +1,8 @@
 import streamlit as st
 
-# st.sidebar.title("sidebar title")
-# st.sidebar.text_input("xxx")
+st.set_page_config(
+    page_title="FullstackGPT Home",
+    page_icon="🥶",
+)
 
-with st.sidebar:
-    st.title("sidebar title")
-    st.text_input("xxx")
-
-st.title("title")
-
-tab_one, tab_two, tab_three = st.tabs(["A", "B", "C"])
-
-with tab_one:
-    st.write("a")
-
-with tab_two:
-    st.write("b")
-
-with tab_three:
-    st.write("c")
+st.title("FullstackGPT Home")
