a6c016e4e33780471d6a4f5037ec93b3e0a3eee2
BenKang <rksk1588@gmail.com>
2024-06-12 00:58:01 +0900

    2024.06.12

--- Program/GPT/fullstack-gpt/Home.py
+++ Program/GPT/fullstack-gpt/Home.py
@@ -1,33 +1,21 @@
 import streamlit as st
-from datetime import datetime
 
+# st.sidebar.title("sidebar title")
+# st.sidebar.text_input("xxx")
 
-today = datetime.today().strftime("%H:%M:%S")
+with st.sidebar:
+    st.title("sidebar title")
+    st.text_input("xxx")
 
-st.title(today)
+st.title("title")
 
+tab_one, tab_two, tab_three = st.tabs(["A", "B", "C"])
 
-model = st.selectbox(
-    "Choose your model",
-    (
-        "GPT-3",
-        "GPT-4",
-    ),
-)
+with tab_one:
+    st.write("a")
 
-if model == "GPT-3":
-    st.write("cheap")
-else:
-    st.write("not cheap")
+with tab_two:
+    st.write("b")
 
-    name = st.text_input("What is your name?")
-
-    st.write(name)
-
-    value = st.slider(
-        "temperature",
-        min_value=0.1,
-        max_value=1.0,
-    )
-
-    st.write(value)
+with tab_three:
+    st.write("c")
