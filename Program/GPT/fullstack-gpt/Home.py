2cdedb2f69cc8d616a2aad314d2f609699b48872
BenKang <rksk1588@gmail.com>
2024-06-13 01:09:44 +0900

    2024.06.13

--- Program/GPT/fullstack-gpt/Home.py
+++ Program/GPT/fullstack-gpt/Home.py
@@ -6,3 +6,20 @@ st.set_page_config(
 )
 
 st.title("FullstackGPT Home")
+
+st.markdown(
+    """
+    # Hello!
+
+    Welcome to my FullstackGPT Portfolio!
+
+    Here are the apps I made:
+
+    - [ ] [DocumentGPT](/DocumentGPT)
+    - [ ] [PrivateGPT](/PrivateGPT)
+    - [ ] [QuizGPT](/QuizGPT)
+    - [ ] [SiteGPT](/SiteGPT)
+    - [ ] [MeetingGPT](/MeetingGPT)
+    - [ ] [InvestorGPT](/InvestorGPT)
+    """
+)
--- Program/GPT/fullstack-gpt/pages/01_DocumentGPT.py
+++ Program/GPT/fullstack-gpt/pages/01_DocumentGPT.py
@@ -1,3 +1,27 @@
+import time
 import streamlit as st
 
+st.set_page_config(
+    page_title="FullstackGPT Home",
+    page_icon="😱",
+)
+
 st.title("DocumentGPT")
+
+
+with st.chat_message("human"):
+    st.write("Helloooooo")
+
+with st.chat_message("ai"):
+    st.write("how are you")
+
+st.chat_input("Send a message to the ai")
+
+with st.status("Embedding file...", expanded=True) as status:
+    time.sleep(2)
+    st.write("Getting the file")
+    time.sleep(2)
+    st.write("Embedding the file")
+    time.sleep(2)
+    st.write("Caching the file")
+    status.update(label="Error", state="error")
