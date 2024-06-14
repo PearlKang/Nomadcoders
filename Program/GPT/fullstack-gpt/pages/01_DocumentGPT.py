cb3ee60937d2361e9c7d89b4dfd0f551ce4d6fac
BenKang <rksk1588@gmail.com>
2024-06-15 03:25:18 +0900

    2024.06.15

--- Program/GPT/fullstack-gpt/pages/01_DocumentGPT.py
+++ Program/GPT/fullstack-gpt/pages/01_DocumentGPT.py
@@ -1,5 +1,10 @@
 import time
 import streamlit as st
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.vectorstores.faiss import FAISS
 
 st.set_page_config(
     page_title="FullstackGPT Home",
@@ -8,31 +13,38 @@ st.set_page_config(
 
 st.title("DocumentGPT")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+st.markdown(
+    """
+Welcome!
 
+Use this chatbot to ask questions to an AI about your files!
+"""
+)
 
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.write(message)
-    if save:
-        st.session_state["messages"].append({"message": message, "role": role})
+file = st.file_uploader(
+    "Upload a .txt .pdf or .docx file",
+    type=["pdf", "txt", "docx"],
+)
 
+if file:
+    st.write(file)
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    st.write(file_content, file_path)
+    with open(file_path, "wb") as f:
+        f.write(file_content)
 
-for message in st.session_state["messages"]:
-    send_message(
-        message["message"],
-        message["role"],
-        save=False,
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
     )
-
-
-message = st.chat_input("Send a message to the ai")
-
-if message:
-    send_message(message, "human")
-    time.sleep(2)
-    send_message(f"You said: {message}", "ai")
-
-    with st.sidebar:
-        st.write(st.session_state)
+    loader = UnstructuredFileLoader("./files/chapter_one.txt")
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    docs = retriever.invoke("ministry of truth")
+    st.write(docs)
