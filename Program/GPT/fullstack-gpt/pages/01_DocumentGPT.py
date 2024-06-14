b8d7f28b973540324d29f9949766ba79e1249755
BenKang <rksk1588@gmail.com>
2024-06-15 03:29:12 +0900

    2024.06.15
    #7.6 Uploading Documents

--- Program/GPT/fullstack-gpt/pages/01_DocumentGPT.py
+++ Program/GPT/fullstack-gpt/pages/01_DocumentGPT.py
@@ -11,6 +11,27 @@ st.set_page_config(
     page_icon="😱",
 )
 
+
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader("./files/chapter_one.txt")
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+
 st.title("DocumentGPT")
 
 st.markdown(
@@ -27,24 +48,6 @@ file = st.file_uploader(
 )
 
 if file:
-    st.write(file)
-    file_content = file.read()
-    file_path = f"./.cache/files/{file.name}"
-    st.write(file_content, file_path)
-    with open(file_path, "wb") as f:
-        f.write(file_content)
-
-    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
-    splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        separator="\n",
-        chunk_size=600,
-        chunk_overlap=100,
-    )
-    loader = UnstructuredFileLoader("./files/chapter_one.txt")
-    docs = loader.load_and_split(text_splitter=splitter)
-    embeddings = OpenAIEmbeddings()
-    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
-    vectorstore = FAISS.from_documents(docs, cached_embeddings)
-    retriever = vectorstore.as_retriever()
-    docs = retriever.invoke("ministry of truth")
-    st.write(docs)
+    retriever = embed_file(file)
+    s = retriever.invoke("winston")
+    s
