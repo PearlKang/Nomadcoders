b9268729d88891aaab4feb020fdf97b7e0062bd5
BenKang <rksk1588@gmail.com>
2024-07-08 21:49:11 +0900

    2024.07.08
    #8 PRIVATEGPT
    #8.0 Introduction

--- Program/GPT/fullstack-gpt/pages/02_PrivateGPT.py
+++ Program/GPT/fullstack-gpt/pages/02_PrivateGPT.py
@@ -12,7 +12,7 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="DocumentGPT",
+    page_title="PrivateGPT",
     page_icon="😱",
 )
 
@@ -43,10 +43,10 @@ llm = ChatOpenAI(
 @st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
-    file_path = f"./.cache/files/{file.name}"
+    file_path = f"./.cache/private_files/{file.name}"
     with open(file_path, "wb") as f:
         f.write(file_content)
-    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    cache_dir = LocalFileStore(f"./.cache/private_embeddings/{file.name}")
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",
         chunk_size=600,
