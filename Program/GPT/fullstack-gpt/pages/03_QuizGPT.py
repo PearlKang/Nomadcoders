9f0bfff6d74d509525318cf3b41eef1be1fae3d0
BenKang <rksk1588@gmail.com>
2024-07-18 22:21:04 +0900

    2024.07.18

--- Program/GPT/fullstack-gpt/pages/03_QuizGPT.py
+++ Program/GPT/fullstack-gpt/pages/03_QuizGPT.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from langchain.retrievers import WikipediaRetriever
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import UnstructuredFileLoader
 
 
 st.set_page_config(
@@ -8,6 +10,23 @@ st.set_page_config(
 )
 
 st.title("QuizGPT")
+
+
+@st.cache_data(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
 
 with st.sidebar:
     choice = st.selectbox(
@@ -22,6 +41,9 @@ with st.sidebar:
             "Upload a .docx, .txt or .pdf file",
             type=["pdf", "txt", "docx"],
         )
+        if file:
+            docs = split_file(file)
+            st.write(docs)
     else:
         topic = st.text_input("Search Wikipedia...")
         if topic:
