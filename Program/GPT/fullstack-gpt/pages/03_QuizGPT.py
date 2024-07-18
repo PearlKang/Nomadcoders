42b0f47db77f7f0b3a3c1c35250efc2cd8e41520
BenKang <rksk1588@gmail.com>
2024-07-18 22:06:29 +0900

    2024.07.18

--- Program/GPT/fullstack-gpt/pages/03_QuizGPT.py
+++ Program/GPT/fullstack-gpt/pages/03_QuizGPT.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from langchain.retrievers import WikipediaRetriever
+
 
 st.set_page_config(
     page_title="QuizGPT",
@@ -6,3 +8,23 @@ st.set_page_config(
 )
 
 st.title("QuizGPT")
+
+with st.sidebar:
+    choice = st.selectbox(
+        "Choose what you want to use.",
+        (
+            "File",
+            "Wikipedia Article",
+        ),
+    )
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload a .docx, .txt or .pdf file",
+            type=["pdf", "txt", "docx"],
+        )
+    else:
+        topic = st.text_input("Search Wikipedia...")
+        if topic:
+            retriever = WikipediaRetriever(top_k_results=5)
+            with st.status("Searching Wikipedia..."):
+                docs = retriever.get_relevant_documents(topic)
