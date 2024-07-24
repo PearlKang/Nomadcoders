4b75a7f6d5fef73fe6fa95b93bb5122f0c91a52e
BenKang <rksk1588@gmail.com>
2024-07-25 01:03:29 +0900

    2024.07.25
    #9.5 Output Parser

--- Program/GPT/fullstack-gpt/pages/03_QuizGPT.py
+++ Program/GPT/fullstack-gpt/pages/03_QuizGPT.py
@@ -1,3 +1,4 @@
+import json
 import streamlit as st
 from langchain.retrievers import WikipediaRetriever
 from langchain.text_splitter import CharacterTextSplitter
@@ -5,7 +6,16 @@ from langchain.document_loaders import UnstructuredFileLoader
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.callbacks import StreamingStdOutCallbackHandler
+from langchain.schema import BaseOutputParser, output_parser
 
+
+class JsonOutputParser(BaseOutputParser):
+    def parse(self, text):
+        text = text.replace("```", "").replace("json", "")
+        return json.loads(text)
+
+
+output_parser = JsonOutputParser()
 
 st.set_page_config(
     page_title="QuizGPT",
@@ -63,13 +73,7 @@ questions_prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-questions_chain = (
-    {
-        "context": format_docs,
-    }
-    | questions_prompt
-    | llm
-)
+questions_chain = {"context": format_docs} | questions_prompt | llm
 
 formatting_prompt = ChatPromptTemplate.from_messages(
     [
@@ -253,11 +257,6 @@ else:
     start = st.button("Generate Quiz")
 
     if start:
-        questions_response = questions_chain.invoke(docs)
-        st.write(questions_response.content)
-        formatting_response = formatting_chain.invoke(
-            {
-                "context": questions_response.content,
-            }
-        )
-        st.write(formatting_response.content)
+        chain = {"context": questions_chain} | formatting_chain | output_parser
+        response = chain.invoke(docs)
+        st.write(response)
