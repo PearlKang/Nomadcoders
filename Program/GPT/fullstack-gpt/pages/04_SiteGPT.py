85d302222c41e19f2439c5e7c68262de8273c4d3
BenKang <rksk1588@gmail.com>
2024-08-19 21:08:29 +0900

    2024.08.19
    #10.4 Map Re Rank Chain

--- Program/GPT/fullstack-gpt/pages/04_SiteGPT.py
+++ Program/GPT/fullstack-gpt/pages/04_SiteGPT.py
@@ -1,8 +1,59 @@
 from langchain.document_loaders import SitemapLoader
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 import streamlit as st
+
+llm = ChatOpenAI(
+    temperature=0.1,
+)
+
+answers_prompt = ChatPromptTemplate.from_template(
+    """
+    Using ONLY the following context answer the user's question. If you can't just say you don't know, don't make anything up.
+
+    Then, give a score to the answer between 0 and 5.
+
+    If the answer answers the user question the score should be high, else it should be low.
+
+    Make sure to always include the answer's score even if it's 0.
+
+    Context: {context}
+
+    Examples:
+
+    Question: How far away is the moon?
+    Answer: The moon is 384,400 km away.
+    Score: 5
+
+    Question: How far away is the sun?
+    Answer: I don't know
+    Score: 0
+
+    Your turn!
+
+    Question: {question}
+"""
+)
+
+
+def get_answers(inputs):
+    docs = inputs["docs"]
+    question = inputs["question"]
+    answers_chain = answers_prompt | llm
+    answers = []
+    for doc in docs:
+        result = answers_chain.invoke(
+            {
+                "question": question,
+                "context": doc.page_content,
+            }
+        )
+        answers.append(result.content)
+    st.write(answers)
 
 
 def parse_page(soup):
@@ -39,6 +90,7 @@ st.set_page_config(
     page_icon="🖥️",
 )
 
+
 st.markdown(
     """
     # SiteGPT
@@ -55,11 +107,17 @@ with st.sidebar:
         placeholder="https://example.com",
     )
 
+
 if url:
     if ".xml" not in url:
         with st.sidebar:
             st.error("Please write down a Sitemap URL.")
     else:
         retriever = load_website(url)
-        docs = retriever.invoke("What is the price of GPT-4 Turbo")
-        docs
+
+        chain = {
+            "docs": retriever,
+            "question": RunnablePassthrough(),
+        } | RunnableLambda(get_answers)
+
+        chain.invoke("What is the pricing of GPT-4 Turbo with vision.")
