f22e35f6dbfa457ce589e0a4a731915c2717037d
BenKang <rksk1588@gmail.com>
2024-11-12 00:36:21 +0900

    2024.11.12
    #13.2 FastAPI Server

--- Program/GPT/fullstack-gpt/main.py
+++ Program/GPT/fullstack-gpt/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 
 app = FastAPI(
@@ -7,8 +8,24 @@ app = FastAPI(
 )
 
 
-@app.get("/quote")
+class Quote(BaseModel):
+    quote: str = Field(
+        description="The quote that Nicolacus Maximus said.",
+    )
+    year: int = Field(
+        description="The year when Nicolacus Maximus said the quote.",
+    )
+
+
+@app.get(
+    "/quote",
+    summary="Returns a random quote by Nicolacus Maximus",
+    description="Upon receiving a GET request this endpoint will return a real quote said by Nicolacus Maximus himself.",
+    response_description="A Quote object that contains the quote said by Nicolacus Maximus and the date when the quote was said.",
+    response_model=Quote,
+)
 def get_quote():
     return {
         "quote": "Life is short so eat it all.",
+        "year": 1950,
     }
